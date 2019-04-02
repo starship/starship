106bcb73bcfe1c3887795a6fa2b106c665df3d56
Matan Kushner <hello@matchai.me>
2019-04-01 23:28:29 -0400

    Add authors to help message

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,11 @@ use clap::App;
 
 fn main() {
     let matches = App::new("Starship")
-                    .about("The cross-platform prompt for astronauts.")
+                    .about("The cross-platform prompt for astronauts. ✨🚀")
                     // pull the version number from Cargo.toml
                     .version(crate_version!())
+                    // pull the authors from Cargo.toml
+                    .author(crate_authors!())
                     .get_matches();
 
 }
