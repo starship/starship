4e1ae0662234bca85837fe136d0ab997b917c835
Matan Kushner <hello@matchai.me>
2019-04-01 23:30:53 -0400

    Run rustfmt

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,10 @@ use clap::App;
 
 fn main() {
     let matches = App::new("Starship")
-                    .about("The cross-platform prompt for astronauts. ✨🚀")
-                    // pull the version number from Cargo.toml
-                    .version(crate_version!())
-                    // pull the authors from Cargo.toml
-                    .author(crate_authors!())
-                    .get_matches();
-
+        .about("The cross-platform prompt for astronauts. ✨🚀")
+        // pull the version number from Cargo.toml
+        .version(crate_version!())
+        // pull the authors from Cargo.toml
+        .author(crate_authors!())
+        .get_matches();
 }
