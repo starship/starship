dc342c12609b79970f70f12b248fc20c409955f4
tim.mulqueen@gmail.com <tim.mulqueen@gmail.com>
2019-04-20 15:23:08 -0400

    fixed formating minus nightly / beta, refactorted order

--- src/modules/rust.rs
+++ src/modules/rust.rs
@@ -4,46 +4,73 @@ use std::fs::{self, DirEntry};
 use crate::context::Context;
 use std::process::Command;
 
-/// Creates a segment with the current Node.js version
+/// Creates a segment with the current Rust version
 ///
-/// Will display the Node.js version if any of the following criteria are met:
-///     - Current directory contains a `.js` file
-///     - Current directory contains a `node_modules` directory
-///     - Current directory contains a `package.json` file
+/// Will display the Rust version if any of the following criteria are met:
+///     - Current directory contains a `.rs` or 'Cargo.toml' file
 pub fn segment(context: &Context) -> Option<Segment> {
-    const RUST_LOGO: &str = "🦀";
-    const SECTION_COLOR: Color = Color::Red;
-
-    let mut segment = Segment::new("node");
     let files = fs::read_dir(&context.current_dir).unwrap();
-
-    // Early return if there are no JS project files
     let is_rs_project = files.filter_map(Result::ok).any(has_rs_files);
     if !is_rs_project {
         return None;
     }
 
-    match Command::new("rustc").arg("-V").output() {
-        Ok(output) => {
-            let version = String::from_utf8(output.stdout).unwrap();
-            segment.set_value(format!("{}  {}", RUST_LOGO, version.trim()))
-        }
-        Err(_) => {
-            return None;
-        }
-    };
+    match get_rust_version() {
+        Some(rust_version) => {
+            const RUST_LOGO: &str = "🦀";
+            const SECTION_COLOR: Color = Color::Red;
 
-    segment.set_style(SECTION_COLOR);
-    Some(segment)
+            let mut segment = Segment::new("rust");
+            segment.set_style(SECTION_COLOR);
+
+            let formated_version = format_rustc_version(rust_version);
+            segment.set_value(format!("{} {}", RUST_LOGO, formated_version));
+
+            return Some(segment)
+        },
+        None => return None
+    };
 }
 
 fn has_rs_files(dir_entry: DirEntry) -> bool {
     let is_rs_file = |d: &DirEntry| -> bool {
         d.path().is_file() && d.path().extension().unwrap_or_default() == "rs"
     };
-    let is_package_json = |d: &DirEntry| -> bool {
+    let is_cargo_toml = |d: &DirEntry| -> bool {
         d.path().is_file() && d.path().file_name().unwrap_or_default() == "Cargo.toml"
     };
 
-    is_rs_file(&dir_entry) || is_package_json(&dir_entry)
+    is_rs_file(&dir_entry) || is_cargo_toml(&dir_entry)
+}
+
+fn get_rust_version() -> Option<String> {
+    match Command::new("rustc").arg("-V").output() {
+        Ok(output) => Some(String::from_utf8(output.stdout).unwrap()),
+        Err(_) => None
+    }
+}
+
+fn format_rustc_version(mut rustc_stdout: String) -> String {
+   let offset = &rustc_stdout.find('(').unwrap();
+   let formated_version: String = rustc_stdout.drain(..offset).collect();
+
+   format!(" v{}", formated_version.replace("rustc", "").trim())
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_format_rustc_version() {
+        let nightly_input = String::from("rustc 1.34.0-nightly (b139669f3 2019-04-10)");
+        assert_eq!(format_rustc_version(nightly_input), " v1.34.0-nightly");
+
+        let beta_input = String::from("rustc 1.34.0-beta.1 (2bc1d406d 2019-04-10)");
+        assert_eq!(format_rustc_version(beta_input), " v1.34.0-beta.1");
+
+        let stable_input = String::from("rustc 1.34.0 (91856ed52 2019-04-10)");
+        assert_eq!(format_rustc_version(stable_input), " v1.34.0");
+    }
 }
