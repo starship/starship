77d9713b683b7517ba47960897fe450a6019249e
Matan Kushner <hello@matchai.me>
2019-04-21 17:53:35 -0400

    Turn dir_files into a vector of PathBufs

--- src/context.rs
+++ src/context.rs
@@ -1,38 +1,41 @@
 use clap::ArgMatches;
 use std::env;
-use std::fs::{self, ReadDir};
+use std::fs;
 use std::path::PathBuf;
 
 pub struct Context<'a> {
     pub current_dir: PathBuf,
-    pub dir_files: ReadDir,
+    pub dir_files: Vec<PathBuf>,
     pub arguments: ArgMatches<'a>,
 }
 
 impl<'a> Context<'a> {
     pub fn new(arguments: ArgMatches) -> Context {
-        // TODO: Currently gets the physical directory. Get the logical directory.
         let current_dir = env::current_dir().expect("Unable to identify current directory.");
-        let dir_files = fs::read_dir(&current_dir).unwrap_or_else(|_| {
-            panic!(
-                "Unable to read current directory: {}",
-                current_dir.to_string_lossy()
-            )
-        });
-
-        Context {
-            current_dir,
-            dir_files,
-            arguments,
-        }
+        Context::new_with_dir(arguments, current_dir)
     }
 
     pub fn new_with_dir<T>(arguments: ArgMatches, dir: T) -> Context
     where
         T: Into<PathBuf>,
     {
+        let current_dir = dir.into();
+
+        // TODO: Currently gets the physical directory. Get the logical directory.
+        let dir_files = fs::read_dir(&current_dir)
+            .unwrap_or_else(|_| {
+                panic!(
+                    "Unable to read current directory: {}",
+                    current_dir.to_string_lossy()
+                )
+            })
+            .filter_map(Result::ok)
+            .map(|entry| entry.path())
+            .collect::<Vec<PathBuf>>();
+
         Context {
-            current_dir: dir.into(),
+            current_dir,
+            dir_files,
             arguments,
         }
     }
--- src/modules/nodejs.rs
+++ src/modules/nodejs.rs
@@ -1,5 +1,5 @@
 use ansi_term::Color;
-use std::fs::DirEntry;
+use std::path::PathBuf;
 use std::process::Command;
 
 use super::Segment;
@@ -18,7 +18,7 @@ pub fn segment(context: &Context) -> Option<Segment> {
     let mut segment = Segment::new("node");
 
     // Early return if there are no JS project files
-    let is_js_project = context.dir_files.filter_map(Result::ok).any(has_js_files);
+    let is_js_project = context.dir_files.iter().any(has_js_files);
     if !is_js_project {
         return None;
     }
@@ -37,15 +37,13 @@ pub fn segment(context: &Context) -> Option<Segment> {
     Some(segment)
 }
 
-fn has_js_files(dir_entry: DirEntry) -> bool {
-    let is_js_file = |d: &DirEntry| -> bool {
-        d.path().is_file() && d.path().extension().unwrap_or_default() == "js"
-    };
-    let is_node_modules = |d: &DirEntry| -> bool {
-        d.path().is_dir() && d.path().file_name().unwrap_or_default() == "node_modules"
-    };
-    let is_package_json = |d: &DirEntry| -> bool {
-        d.path().is_file() && d.path().file_name().unwrap_or_default() == "package.json"
+fn has_js_files(dir_entry: &PathBuf) -> bool {
+    let is_js_file =
+        |d: &PathBuf| -> bool { d.is_file() && d.extension().unwrap_or_default() == "js" };
+    let is_node_modules =
+        |d: &PathBuf| -> bool { d.is_dir() && d.file_name().unwrap_or_default() == "node_modules" };
+    let is_package_json = |d: &PathBuf| -> bool {
+        d.is_file() && d.file_name().unwrap_or_default() == "package.json"
     };
 
     is_js_file(&dir_entry) || is_node_modules(&dir_entry) || is_package_json(&dir_entry)
