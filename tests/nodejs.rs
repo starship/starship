36264840d9d066b6d312e55b2d6652bb18fbb23c
Matan Kushner <hello@matchai.me>
2019-04-26 13:17:26 -0400

    Expand existing node tests

--- tests/nodejs.rs
+++ tests/nodejs.rs
@@ -1,6 +1,6 @@
 use ansi_term::Color;
 use starship::segment::Segment;
-use std::fs::File;
+use std::fs::{self, File};
 use std::io;
 use tempfile::TempDir;
 
@@ -9,14 +9,47 @@ mod common;
 #[test]
 #[ignore]
 fn folder_with_package_json() -> io::Result<()> {
-    let project_dir = TempDir::new()?;
-    File::create(project_dir.path().join("package.json"))?;
+    let dir = TempDir::new()?;
+    File::create(dir.path().join("package.json"))?;
 
     let expected = Segment::new("node")
         .set_value("⬢ v12.0.0")
         .set_style(Color::Green)
         .output();
-    let actual = common::render_segment("nodejs", &project_dir.path());
+    let actual = common::render_segment("nodejs", &dir.path());
+    assert_eq!(expected, actual);
+
+    Ok(())
+}
+
+#[test]
+#[ignore]
+fn folder_with_js_file() -> io::Result<()> {
+    let dir = TempDir::new()?;
+    File::create(dir.path().join("index.js"))?;
+
+    let expected = Segment::new("node")
+        .set_value("⬢ v12.0.0")
+        .set_style(Color::Green)
+        .output();
+    let actual = common::render_segment("nodejs", &dir.path());
+    assert_eq!(expected, actual);
+
+    Ok(())
+}
+
+#[test]
+#[ignore]
+fn folder_with_node_modules() -> io::Result<()> {
+    let dir = TempDir::new()?;
+    let node_modules = dir.path().join("node_modules");
+    fs::create_dir_all(&node_modules)?;
+    
+    let expected = Segment::new("node")
+        .set_value("⬢ v12.0.0")
+        .set_style(Color::Green)
+        .output();
+    let actual = common::render_segment("nodejs", &dir.path());
     assert_eq!(expected, actual);
 
     Ok(())
