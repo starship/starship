83bd802c344072363881aa39afcfaf63b65df3fa
John Letey <john.letey@colorado.edu>
2019-04-28 20:44:26 +0100

    Add integration tests for Rust segment

---
+++ tests/rust.rs
@@ -0,0 +1,39 @@
+use ansi_term::Color;
+use starship::segment::Segment;
+use std::fs::File;
+use std::io;
+use tempfile::TempDir;
+
+mod common;
+
+#[test]
+#[ignore]
+fn folder_with_cargo_toml() -> io::Result<()> {
+    let dir = TempDir::new()?;
+    File::create(dir.path().join("Cargo.toml"))?;
+
+    let expected = Segment::new("rust")
+        .set_value("🦀 v1.34.1")
+        .set_style(Color::Red.bold())
+        .output();
+    let actual = common::render_segment("rust", &dir.path());
+    assert_eq!(expected, actual);
+
+    Ok(())
+}
+
+#[test]
+#[ignore]
+fn folder_with_rs_file() -> io::Result<()> {
+    let dir = TempDir::new()?;
+    File::create(dir.path().join("main.rs"))?;
+
+    let expected = Segment::new("rust")
+        .set_value("🦀 v1.34.1")
+        .set_style(Color::Red.bold())
+        .output();
+    let actual = common::render_segment("rust", &dir.path());
+    assert_eq!(expected, actual);
+
+    Ok(())
+}
