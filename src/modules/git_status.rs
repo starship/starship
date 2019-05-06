875e2547ff4d079aa89f5b8b3cb07d6d79a5bf0e
Matan Kushner <hello@matchai.me>
2019-05-05 23:57:24 -0400

    Add logic to check for stashed files

--- src/modules/git_status.rs
+++ src/modules/git_status.rs
@@ -12,6 +12,8 @@ pub fn segment(context: &Context) -> Option<Module> {
     let repository = context.repository.as_ref()?;
     let repo_status = get_repo_status(repository)?;
 
+    debug!("Repo status: {:?}", repo_status);
+
     let mut module = Module::new("git_status");
     module.get_prefix().set_value("[").set_style(module_style);
     module.get_suffix().set_value("] ").set_style(module_style);
@@ -25,27 +27,30 @@ pub fn segment(context: &Context) -> Option<Module> {
     const GIT_STATUS_STASHED: &str = "$";
     const GIT_STATUS_UNMERGED: &str = "=";
 
-    if repo_status.is_wt_new() {
-        module.new_segment("untracked", GIT_STATUS_UNTRACKED);
-    }
-
-    if repo_status.is_index_modified() {
-        module.new_segment("staged", GIT_STATUS_ADDED);
-    }
-
-    if repo_status.is_wt_modified() {
-        module.new_segment("modified", GIT_STATUS_MODIFIED);
-    }
-
-    if repo_status.is_wt_renamed() || repo_status.is_index_renamed() {
-        module.new_segment("renamed", GIT_STATUS_RENAMED);
+    if repository.revparse_single("refs/stash").is_ok() {
+        module.new_segment("stashed", GIT_STATUS_STASHED);
     }
 
     if repo_status.is_wt_deleted() || repo_status.is_index_deleted() {
         module.new_segment("deleted", GIT_STATUS_DELETED);
     }
 
-    // TODO: Check for stashed files
+    if repo_status.is_wt_renamed() || repo_status.is_index_renamed() {
+        module.new_segment("renamed", GIT_STATUS_RENAMED);
+    }
+
+    if repo_status.is_wt_modified() {
+        module.new_segment("modified", GIT_STATUS_MODIFIED);
+    }
+
+    if repo_status.is_index_modified() || repo_status.is_index_new() {
+        module.new_segment("staged", GIT_STATUS_ADDED);
+    }
+
+    if repo_status.is_wt_new() {
+        module.new_segment("untracked", GIT_STATUS_UNTRACKED);
+    }
+
     // TODO: Check for unmerged changes
     // TODO: Check whether branch is ahead
     // TODO: Check whether branch is behind
