87404382b8fc392c9b768f0715a8f177ec911c6f
Matan Kushner <hello@matchai.me>
2019-07-18 22:58:52 -0400

    feat: Improve configuration for battery

--- src/modules/battery.rs
+++ src/modules/battery.rs
@@ -28,13 +28,13 @@ pub fn module<'a>(context: &'a Context) -> Option<Module<'a>> {
 
     match state {
         battery::State::Full => {
-            module.new_segment("full", BATTERY_FULL);
+            module.new_segment("full_symbol", BATTERY_FULL);
         }
         battery::State::Charging => {
-            module.new_segment("charging", BATTERY_CHARGING);
+            module.new_segment("charging_symbol", BATTERY_CHARGING);
         }
         battery::State::Discharging => {
-            module.new_segment("discharging", BATTERY_DISCHARGING);
+            module.new_segment("discharging_symbol", BATTERY_DISCHARGING);
         }
         _ => return None,
     }
