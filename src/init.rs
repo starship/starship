3720ad56dd49a88211c1fe130f0a2e723c6ed73e
Kevin Song <chipbuster@gmail.com>
2019-07-26 03:06:30 -0500

    Add tests for cmd_duration module while bugfixing init

--- src/init.rs
+++ src/init.rs
@@ -1,57 +1,26 @@
 use std::ffi::OsStr;
 use std::path::Path;
 
+/* We need to send execution time to the prompt for the timer module. For fish,
+this is fairly straightforward. For bash and zsh, we'll need to use several
+shell utilities to get the time, as well as render the prompt */
+
 pub fn init(shell_name: &str) {
     log::debug!("Shell name: {}", shell_name);
 
     let shell_basename = Path::new(shell_name).file_stem().and_then(OsStr::to_str);
 
     let setup_script = match shell_basename {
-        // The contents of `PROMPT_COMMAND` are executed as a regular Bash command
-        // just before Bash displays a prompt.
         Some("bash") => {
-            let script = r##"
-            starship_preexec() {
-                if [ "$PREEXEC_READY" = "true" ]; then
-                    PREEXEC_READY=false;
-                    STARSHIP_START_TIME=$(date +%s);
-                fi
-            };
-
-            starship_prompt() {
-                PREEXEC_READY=true;
-                STARSHIP_END_TIME=$(date +%s);
-                ELAPSED=$((STARSHIP_END_TIME - STARSHIP_START_TIME));
-                PS1="$(starship prompt --status=$? --elapsed=$ELAPSED)";
-            };
- 
-            trap starship_preexec DEBUG;
-            PROMPT_COMMAND=starship_prompt;
-            "##;
+            let script = BASH_INIT;
             Some(script)
         }
-        /* `precmd` executes a command before the zsh prompt is displayed.
-        We need to set STARSHIP_START_TIME as part of the init commmand or
-        else the first prompt will display a very large time */
         Some("zsh") => {
-            let script = r##"
-            precmd() {
-                STARSHIP_END_TIME="$(date +%s)"
-                ELAPSED=$((STARSHIP_END_TIME - STARSHIP_START_TIME))
-                PROMPT="$(starship prompt --status=$? --elapsed=$ELAPSED)"
-            };
-            preexec(){
-                STARSHIP_START_TIME="$(date +%s)"
-            };
-            STARSHIP_START_TIME="$(date +%s)"
-            "##;
+            let script = ZSH_INIT;
             Some(script)
         }
         Some("fish") => {
-            let script = r##"function fish_prompt;
-            set -l elapsed (math --scale=0 "$CMD_DURATION / 1000");
-            starship prompt --status=$status --elapsed=$elapsed;
-            end;"##;
+            let script = FISH_INIT;
             Some(script)
         }
         None => {
@@ -82,3 +51,94 @@ pub fn init(shell_name: &str) {
         print!("{}", script);
     }
 }
+
+/* For bash: we need to manually hook functions ourself: PROMPT_COMMAND will exec
+   right before the prompt is drawn, and any function trapped by DEBUG will exec
+   before a command is run. There is no way to avoid clobbering these functions,
+   so we will not hook them if they are already defined.
+
+   There is a preexec/precmd framework for bash out there: if we find the
+   appropriate variables set, assume we are using that framework:
+   https://github.com/rcaloras/bash-preexec
+
+   Bash quirk: DEBUG is triggered whenever a command is executed, even if that
+   command is part of a pipeline. To avoid only timing the last part of a pipeline,
+   we only start the timer if no timer has been started since the last prompt draw,
+   tracked by the variable PREEXEC_READY. Similarly, only draw timing info if
+   STARSHIP_START_TIME is defined, in case preexec was interrupted.
+*/
+
+const BASH_INIT: &str = r##"
+starship_preexec() {
+    if [ "$PREEXEC_READY" = "true" ]; then
+        PREEXEC_READY=false;
+        STARSHIP_START_TIME=$(date +%s);
+    fi
+};
+starship_precmd() {
+    if [[ $STARSHIP_START_TIME ]]; then
+        STARSHIP_END_TIME=$(date +%s);
+        STARSHIP_ELAPSED=$((STARSHIP_END_TIME - STARSHIP_START_TIME));
+        PS1="$(starship prompt --status=$? --elapsed=$STARSHIP_ELAPSED)";
+        unset STARSHIP_START_TIME;
+    else
+        PS1="$(starship prompt --status=$?)";
+    fi
+    PREEXEC_READY=true;
+};
+# Check for preexec/precmd frameworks
+if [[ $preexec_functions ]]; then
+    preexec_functions+=(starship_preexec);
+    precmd_functions+=(starship_precmd);
+    STARSHIP_START_TIME=$(date +%s);
+fi
+# Check if it is safe to trap DEBUG: get name of function trapping DEBUG, if any
+dbg_trap="$(trap -p DEBUG | cut -d' ' -f3 | tr -d \')"  
+if [[ -z $dbg_trap || "$dbg_trap" = "starship_preexec" ]]; then
+    trap starship_preexec DEBUG;
+    PROMPT_COMMAND=starship_precmd;
+    STARSHIP_START_TIME=$(date +%s);
+else
+    PROMPT_COMMAND=starship_precmd;
+fi
+"##;
+//TODO: How to warn the user?
+
+/* For zsh: preexec_functions and precmd_functions provide preexec/precmd in a
+   way that lets us avoid clobbering them.
+
+   Zsh quirk: preexec() is only fired if a command is actually run (unlike in
+   bash, where spamming empty commands still triggers DEBUG). This means a user
+   spamming ENTER at an empty command line will see increasing runtime (since
+   preexec never actually fires to reset the start time).
+
+   To fix this, only pass the time if STARSHIP_START_TIME is defined, and unset
+   it after passing the time, so that we only measure actual commands.
+*/
+
+const ZSH_INIT: &str = r##"
+starship_precmd() {
+    if [[ $STARSHIP_START_TIME ]]; then
+        STARSHIP_END_TIME="$(date +%s)";
+        ELAPSED=$((STARSHIP_END_TIME - STARSHIP_START_TIME));
+        PROMPT="$(starship prompt --status=$? --elapsed=$ELAPSED)";
+        unset STARSHIP_START_TIME;
+    else
+        PROMPT="$(starship prompt --status=$?)";
+    fi
+};
+starship_preexec(){
+    STARSHIP_START_TIME="$(date +%s)"
+};
+precmd_functions+=(starship_precmd);
+preexec_functions+=(starship_preexec);
+STARSHIP_START_TIME="$(date +%s)";
+"##;
+
+/* Fish setup is simple because they give us CMD_DURATION. Hooray! */
+const FISH_INIT: &str = r##"
+function fish_prompt;
+    set -l elapsed (math --scale=0 "$CMD_DURATION / 1000");
+    starship prompt --status=$status --elapsed=$elapsed;
+end;
+"##;
--- src/modules/timer.rs
+++ src/modules/timer.rs
@@ -1,5 +1,4 @@
 use ansi_term::Color;
-use std::path::Path;
 
 use super::{Context, Module};
 
@@ -7,10 +6,11 @@ use super::{Context, Module};
 ///
 /// Will only print if last command took more than two seconds to execute.
 pub fn module<'a>(context: &'a Context) -> Option<Module<'a>> {
+    let mut module = context.new_module("timer")?;
     let arguments = &context.arguments;
     let elapsed = arguments
         .value_of("elapsed_time")
-        .unwrap_or("0")
+        .unwrap_or("x") // not a time
         .parse::<u64>()
         .ok()?;
 
@@ -19,16 +19,15 @@ pub fn module<'a>(context: &'a Context) -> Option<Module<'a>> {
         _ => Color::Yellow.bold(),
     };
 
-    let mut module = context.new_module("timer")?;
     module.set_style(module_color);
-    module.new_segment("timer", &format!("took {}", render_time(&elapsed)));
+    module.new_segment("timer", &format!("took {}", render_time(elapsed)));
     module.get_prefix().set_value("");
 
     Some(module)
 }
 
 /// Render the time into a nice human-readable string
-fn render_time(raw_seconds: &u64) -> String {
+fn render_time(raw_seconds: u64) -> String {
     // Calculate a simple breakdown into days/hours/minutes/seconds
     let (seconds, raw_minutes) = (raw_seconds % 60, raw_seconds / 60);
     let (minutes, raw_hours) = (raw_minutes % 60, raw_minutes / 60);
@@ -50,5 +49,21 @@ fn render_time_component((component, suffix): (&u64, &&str)) -> String {
     match component {
         0 => String::new(),
         n => format!("{}{}", n, suffix),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_10s() {
+        assert_eq!(render_time(10 as u64), "10s")
+    }
+    fn test_90s() {
+        assert_eq!(render_time(90 as u64), "1m30s")
+    }
+    fn test_10110s() {
+        assert_eq!(render_time(10110 as u64), "1h48m30s")
     }
 }
