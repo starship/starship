6b804ce94f68bad83b1b70e7094d05ce96c0b4a5
John Letey <30328854+johnletey@users.noreply.github.com>
2019-08-10 09:40:14 +0100

    Add jobs testsuite

---
+++ tests/testsuite/jobs.rs
@@ -0,0 +1,91 @@
+use ansi_term::Color;
+use std::fs;
+use std::io;
+use std::path::Path;
+use tempfile::TempDir;
+
+use crate::common::{self, TestCommand};
+
+#[test]
+fn config_blank_job_0() -> io::Result<()> {
+    let output = common::render_module("jobs")
+        .arg("--jobs=0")
+        .output()?;
+    let actual = String::from_utf8(output.stdout).unwrap();
+
+    let expected = "";
+    assert_eq!(expected, actual);
+    Ok(())
+}
+
+#[test]
+fn config_blank_job_1() -> io::Result<()> {
+    let output = common::render_module("jobs")
+        .arg("--jobs=1")
+        .output()?;
+    let actual = String::from_utf8(output.stdout).unwrap();
+
+    let expected = format!("{} ", Color::Blue.bold().paint("✦"));
+    assert_eq!(expected, actual);
+    Ok(())
+}
+
+#[test]
+fn config_blank_job_2() -> io::Result<()> {
+    let output = common::render_module("jobs")
+        .arg("--jobs=2")
+        .output()?;
+    let actual = String::from_utf8(output.stdout).unwrap();
+
+    let expected = format!("{} ", Color::Blue.bold().paint("✦ 2"));
+    assert_eq!(expected, actual);
+    Ok(())
+}
+
+#[test]
+fn config_2_job_2() -> io::Result<()> {
+    let output = common::render_module("jobs")
+        .use_config(toml::toml! {
+            [jobs]
+            threshold = 2
+        })
+        .arg("--jobs=2")
+        .output()?;
+    let actual = String::from_utf8(output.stdout).unwrap();
+
+    let expected = format!("{} ", Color::Blue.bold().paint("✦"));
+    assert_eq!(expected, actual);
+    Ok(())
+}
+
+#[test]
+fn config_2_job_3() -> io::Result<()> {
+    let output = common::render_module("jobs")
+        .use_config(toml::toml! {
+            [jobs]
+            threshold = 2
+        })
+        .arg("--jobs=3")
+        .output()?;
+    let actual = String::from_utf8(output.stdout).unwrap();
+
+    let expected = format!("{} ", Color::Blue.bold().paint("✦ 3"));
+    assert_eq!(expected, actual);
+    Ok(())
+}
+
+#[test]
+fn config_disabled() -> io::Result<()> {
+    let output = common::render_module("jobs")
+        .use_config(toml::toml! {
+            [cmd_duration]
+            disabled = true
+        })
+        .arg("--jobs=1")
+        .output()?;
+    let actual = String::from_utf8(output.stdout).unwrap();
+
+    let expected = "";
+    assert_eq!(expected, actual);
+    Ok(())
+}
