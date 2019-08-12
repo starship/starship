6c430c138e5cfbfffd73c3cdc6dbbac4b3db384b
John Letey <30328854+johnletey@users.noreply.github.com>
2019-08-12 08:40:59 +0100

    Correct Fish syntax
    Co-Authored-By: Matan Kushner <hello@matchai.me>

--- src/init.rs
+++ src/init.rs
@@ -108,6 +108,6 @@ function fish_prompt;
     set -l exit_code $status;
     set -l CMD_DURATION "$CMD_DURATION$cmd_duration";
     set -l starship_duration (math --scale=0 "$CMD_DURATION / 1000");
-    starship prompt --status=$exit_code --cmd-duration=$starship_duration --jobs=$(count (jobs -p));
+    starship prompt --status=$exit_code --cmd-duration=$starship_duration --jobs=(count (jobs -p));
 end;
 "##;
