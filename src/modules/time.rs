94b8670850a53079fd74be1cbca74b164ee2476e
John Letey <30328854+johnletey@users.noreply.github.com>
2019-08-12 08:58:55 +0100

    Fix error when retrieving config time format

--- src/modules/time.rs
+++ src/modules/time.rs
@@ -7,17 +7,17 @@ use super::{Context, Module};
 pub fn module<'a>(context: &'a Context) -> Option<Module<'a>> {
     let mut module = context.new_module("time")?;
 
-    let time_format = module.config_value_str("format").unwrap_or("%H:%M");
+    let mut time_format = module.config_value_str("format").unwrap_or("%H:%M").to_owned();
 
     let is_12hr = module.config_value_bool("12hr").unwrap_or(false);
-    if is_12hr {
-        let time_format = module.config_value_str("format").unwrap_or("%I:%M %p");
+    if is_12hr == true {
+        time_format = module.config_value_str("format").unwrap_or("%I:%M %p").to_owned();
     }
 
     let local: DateTime<Local> = Local::now();
 
     module.set_style(Color::Yellow.bold());
-    module.new_segment("time", &format!("[{}]", local.format(time_format)));
+    module.new_segment("time", &format!("[{}]", local.format(&time_format)));
     module.get_prefix().set_value("");
 
     Some(module)
