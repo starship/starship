2e388624af23d2fbd59f047654672b870422d561
John Letey <30328854+johnletey@users.noreply.github.com>
2019-08-15 11:53:53 +0100

    Add another test

--- tests/testsuite/time.rs
+++ tests/testsuite/time.rs
@@ -7,6 +7,21 @@ use tempfile::TempDir;
 use crate::common::{self, TestCommand};
 
 #[test]
+fn config_enabled() -> io::Result<()> {
+    let output = common::render_module("time")
+        .use_config(toml::toml! {
+            [time]
+            disabled = false
+        })
+        .output()?;
+    let actual = String::from_utf8(output.stdout).unwrap();
+
+    let expected = "";
+    assert_eq!(expected, actual);
+    Ok(())
+}
+
+#[test]
 fn config_blank() -> io::Result<()> {
     let output = common::render_module("time").output()?;
     let actual = String::from_utf8(output.stdout).unwrap();
