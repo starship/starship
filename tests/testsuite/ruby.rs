121191224ae2e4bd9825da4e3ec5754ec9b3c71e
Matan Kushner <hello@matchai.me>
2019-08-23 17:31:13 -0400

    Use choco for Ruby install on Windows

--- tests/testsuite/ruby.rs
+++ tests/testsuite/ruby.rs
@@ -31,7 +31,7 @@ fn folder_with_gemfile() -> io::Result<()> {
         .output()?;
     let actual = String::from_utf8(output.stdout).unwrap();
 
-    let expected = format!("via {} ", Color::Red.bold().paint("💎 v2.6.2"));
+    let expected = format!("via {} ", Color::Red.bold().paint("💎 v2.5.5"));
     assert_eq!(expected, actual);
     Ok(())
 }
@@ -48,7 +48,7 @@ fn folder_with_rb_file() -> io::Result<()> {
         .output()?;
     let actual = String::from_utf8(output.stdout).unwrap();
 
-    let expected = format!("via {} ", Color::Red.bold().paint("💎 v2.6.2"));
+    let expected = format!("via {} ", Color::Red.bold().paint("💎 v2.5.5"));
     assert_eq!(expected, actual);
     Ok(())
 }
