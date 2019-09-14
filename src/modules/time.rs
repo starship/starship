7e891f17c1c6f59f7a8be18e83927271f307dd3d
Nick Young <nick@nickwb.net>
2019-09-15 00:23:53 +1000

    perf: Lazy load files from directory (#335)
    Changes context to use `once_cell` to lazily evaluate directory listing on first use.

--- src/modules/time.rs
+++ src/modules/time.rs
@@ -41,8 +41,8 @@ pub fn module<'a>(context: &'a Context) -> Option<Module<'a>> {
 
 /// Format a given time into the given string. This function should be referentially
 /// transparent, which makes it easy to test (unlike anything involving the actual time)
-fn format_time(time_format: &str, localtime: DateTime<Local>) -> String {
-    localtime.format(time_format).to_string()
+fn format_time(time_format: &str, local_time: DateTime<Local>) -> String {
+    local_time.format(time_format).to_string()
 }
 
 /* Because we cannot make acceptance tests for the time module, these unit
