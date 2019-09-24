56d7903d4ff41447398e2902ad265934bcc8d3f5
heyrict <xiezh0831@126.com>
2019-09-24 20:51:13 +0800

    Implement from_config for derive macro

--- starship_module_config_derive/src/lib.rs
+++ starship_module_config_derive/src/lib.rs
@@ -11,40 +11,66 @@ pub fn derive_module_config(input: TokenStream) -> TokenStream {
     impl_module_config(dinput)
 }
 
-fn impl_module_config(dinput: DeriveInput) -> TokenStream {
+fn impl_module_config(dinput: DeriveInput) -> proc_macro::TokenStream {
     let struct_ident = &dinput.ident;
     let (_impl_generics, ty_generics, where_clause) = dinput.generics.split_for_impl();
 
+    let mut from_config = quote! {};
+    let mut load_config = quote! {};
+
     if let syn::Data::Struct(data) = dinput.data {
         if let syn::Fields::Named(fields_named) = data.fields {
-            let mut tokens = quote! {};
+            let mut load_tokens = quote! {};
+            let mut from_tokens = quote! {};
 
             for field in fields_named.named.iter() {
                 let ident = field.ident.as_ref().unwrap();
-                let _ty = &field.ty;
-                let new_tokens = quote! {
+                let ty = &field.ty;
+
+                let new_load_tokens = quote! {
                     if let Some(config_str) = config.get(stringify!(#ident)) {
                         new_module_config.#ident = new_module_config.#ident.load_config(config_str);
                     }
                 };
-                tokens = quote! {
-                    #tokens
-                    #new_tokens
+                let new_from_tokens = quote! {
+                    #ident: <#ty>::from_config(config.get(stringify!(#ident))?)?,
                 };
+
+                load_tokens = quote! {
+                    #load_tokens
+                    #new_load_tokens
+                };
+                from_tokens = quote! {
+                    #from_tokens
+                    #new_from_tokens
+                }
             }
 
-            return TokenStream::from(quote! {
-                impl<'a> ModuleConfig<'a> for #struct_ident #ty_generics #where_clause {
-                    fn load_config(&self, config: &'a toml::Value) -> Self {
-                        let mut new_module_config = self.clone();
-                        if let toml::Value::Table(config) = config {
-                            #tokens
-                        }
-                        new_module_config
+            load_config = quote! {
+                fn load_config(&self, config: &'a toml::Value) -> Self {
+                    let mut new_module_config = self.clone();
+                    if let toml::Value::Table(config) = config {
+                        #load_tokens
                     }
+                    new_module_config
                 }
-            });
+            };
+            from_config = quote! {
+                fn from_config(config: &'a toml::Value) -> Option<Self> {
+                    let config = config.as_table()?;
+
+                    Some(#struct_ident {
+                        #from_tokens
+                    })
+                }
+            };
         }
     }
-    TokenStream::from(quote! {})
+
+    TokenStream::from(quote! {
+        impl<'a> ModuleConfig<'a> for #struct_ident #ty_generics #where_clause {
+            #from_config
+            #load_config
+        }
+    })
 }
