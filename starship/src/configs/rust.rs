c39506c6f68e91f8967e3cb3db4e0255ec5ec81d
Zhenhui Xie <xiezh0831@126.com>
2019-09-30 22:56:05 +0800

    chore: Cleanup after the config refactor (#453)

--- starship/src/configs/rust.rs
+++ starship/src/configs/rust.rs
@@ -11,34 +11,6 @@ pub struct RustConfig<'a> {
     pub disabled: bool,
 }
 
-/* This is what the macro adds.
-impl<'a> ModuleConfig<'a> for RustConfig<'a> {
-    fn load_config(&self, config: &'a toml::Value) -> Self {
-        let mut new_module_config = self.clone();
-        if let toml::Value::Table(config) = config {
-            if let Some(config_str) = config.get("symbol") {
-                new_module_config.symbol = new_module_config.symbol.load_config(config_str);
-            }
-            if let Some(config_str) = config.get("disabled") {
-                new_module_config.disabled = new_module_config.disabled.load_config(config_str);
-            }
-            if let Some(config_str) = config.get("style") {
-                new_module_config.style = new_module_config.style.load_config(config_str);
-            }
-        }
-        new_module_config
-    }
-    fn from_config(config: &'a toml::Value) -> Option<Self> {
-        let config = config.as_table()?;
-        Some(RustConfig {
-            symbol: <&'a str>::from_config(config.get("symbol")?)?,
-            style: <Style>::from_config(config.get("style")?)?,
-            disabled: <bool>::from_config(config.get("disabled")?)?,
-        })
-    }
-}
-*/
-
 impl<'a> RootModuleConfig<'a> for RustConfig<'a> {
     fn new() -> Self {
         RustConfig {
