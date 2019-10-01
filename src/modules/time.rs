48fc18318cd8c08881f6267c1904ed715d67c064
Keith Wade <keawade@protonmail.com>
2019-09-30 20:06:44 -0500

    Clean up

--- src/modules/time.rs
+++ src/modules/time.rs
@@ -39,7 +39,7 @@ pub fn module<'a>(context: &'a Context) -> Option<Module<'a>> {
 
     let formatted_time_string: String;
     if utc_time_offset_str == "local" {
-        formatted_time_string = format_time_local(&time_format, local_time);
+        formatted_time_string = format_time(&time_format, local_time);
     } else {
         let utc_time = DateTime::<Utc>::from_utc(local_time.naive_utc(), Utc);
         log::trace!("UTC time now is {}", utc_time);
@@ -52,10 +52,10 @@ pub fn module<'a>(context: &'a Context) -> Option<Module<'a>> {
             let target_time = utc_time.with_timezone(&timezone_offset);
             log::trace!("Time in target timezone now is {}", target_time);
 
-            formatted_time_string = format_time_fo(&time_format, target_time);
+            formatted_time_string = format_time_fixed_offset(&time_format, target_time);
         } else {
             // Fall back to local time if an invalid offset is provided.
-            formatted_time_string = format_time_local(&time_format, local_time);
+            formatted_time_string = format_time(&time_format, local_time);
         }
     }
 
@@ -68,11 +68,11 @@ pub fn module<'a>(context: &'a Context) -> Option<Module<'a>> {
 
 /// Format a given time into the given string. This function should be referentially
 /// transparent, which makes it easy to test (unlike anything involving the actual time)
-fn format_time_local(time_format: &str, local_time: DateTime<Local>) -> String {
+fn format_time(time_format: &str, local_time: DateTime<Local>) -> String {
     local_time.format(time_format).to_string()
 }
 
-fn format_time_fo(time_format: &str, utc_time: DateTime<FixedOffset>) -> String {
+fn format_time_fixed_offset(time_format: &str, utc_time: DateTime<FixedOffset>) -> String {
     utc_time.format(time_format).to_string()
 }
 
@@ -89,49 +89,49 @@ mod tests {
     #[test]
     fn test_midnight_12hr() {
         let time = Local.ymd(2014, 7, 8).and_hms(0, 0, 0);
-        let formatted = format_time_local(FMT_12, time);
+        let formatted = format_time(FMT_12, time);
         assert_eq!(formatted, "12:00:00 AM");
     }
 
     #[test]
     fn test_midnight_24hr() {
         let time = Local.ymd(2014, 7, 8).and_hms(0, 0, 0);
-        let formatted = format_time_local(FMT_24, time);
+        let formatted = format_time(FMT_24, time);
         assert_eq!(formatted, "00:00:00");
     }
 
     #[test]
     fn test_noon_12hr() {
         let time = Local.ymd(2014, 7, 8).and_hms(12, 0, 0);
-        let formatted = format_time_local(FMT_12, time);
+        let formatted = format_time(FMT_12, time);
         assert_eq!(formatted, "12:00:00 PM");
     }
 
     #[test]
     fn test_noon_24hr() {
         let time = Local.ymd(2014, 7, 8).and_hms(12, 0, 0);
-        let formatted = format_time_local(FMT_24, time);
+        let formatted = format_time(FMT_24, time);
         assert_eq!(formatted, "12:00:00");
     }
 
     #[test]
     fn test_arbtime_12hr() {
         let time = Local.ymd(2014, 7, 8).and_hms(15, 36, 47);
-        let formatted = format_time_local(FMT_12, time);
+        let formatted = format_time(FMT_12, time);
         assert_eq!(formatted, "03:36:47 PM");
     }
 
     #[test]
     fn test_arbtime_24hr() {
         let time = Local.ymd(2014, 7, 8).and_hms(15, 36, 47);
-        let formatted = format_time_local(FMT_24, time);
+        let formatted = format_time(FMT_24, time);
         assert_eq!(formatted, "15:36:47");
     }
 
     #[test]
     fn test_format_with_paren() {
         let time = Local.ymd(2014, 7, 8).and_hms(15, 36, 47);
-        let formatted = format_time_local("[%T]", time);
+        let formatted = format_time("[%T]", time);
         assert_eq!(formatted, "[15:36:47]");
     }
 }
