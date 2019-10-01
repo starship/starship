5b89bc1780db915c0466463d8d8ce8a71ad0dc1e
heyrict <xiezh0831@126.com>
2019-10-01 08:18:26 +0800

    refactor: Rewrite aws module to use module config

---
+++ starship/src/configs/aws.rs
@@ -0,0 +1,29 @@
+use crate::config::{ModuleConfig, RootModuleConfig, SegmentConfig};
+
+use ansi_term::{Color, Style};
+use starship_module_config_derive::ModuleConfig;
+
+#[derive(Clone, ModuleConfig)]
+pub struct AwsConfig<'a> {
+    pub symbol: SegmentConfig<'a>,
+    pub profile: SegmentConfig<'a>,
+    pub style: Style,
+    pub disabled: bool,
+}
+
+impl<'a> RootModuleConfig<'a> for AwsConfig<'a> {
+    fn new() -> Self {
+        AwsConfig {
+            symbol: SegmentConfig {
+                value: "☁️ ",
+                style: None,
+            },
+            profile: SegmentConfig {
+                value: "",
+                style: None,
+            },
+            style: Color::Yellow.bold(),
+            disabled: false,
+        }
+    }
+}
--- starship/src/configs/mod.rs
+++ starship/src/configs/mod.rs
@@ -1,3 +1,4 @@
+pub mod aws;
 pub mod battery;
 pub mod rust;
 
