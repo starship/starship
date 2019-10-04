14aa77c912b1c82d838f1efb042ed0786fc5f122
Zach Mertes <zmertes@purdue.edu>
2019-10-04 12:58:54 -0400

    Add tests for hostname truncation

--- tests/testsuite/hostname.rs
+++ tests/testsuite/hostname.rs
@@ -97,6 +97,47 @@ fn suffix() -> io::Result<()> {
     Ok(())
 }
 
+#[test]
+fn no_trim_end() -> io::Result<()> {
+    let hostname = match get_hostname() {
+        Some(h) => h,
+        None => return hostname_not_tested(),
+    };
+    let output = common::render_module("hostname")
+        .env_clear()
+        .use_config(toml::toml! {
+            [hostname]
+            ssh_only = false
+            trim_end = ""
+        })
+        .output()?;
+    let actual = String::from_utf8(output.stdout).unwrap();
+    let expected = format!("on {} ", style().paint(hostname));
+    assert_eq!(expected, actual);
+    Ok(())
+}
+
+#[test]
+fn trim_end() -> io::Result<()> {
+    let hostname = match get_hostname() {
+        Some(h) => h,
+        None => return hostname_not_tested(),
+    };
+    let (remainder, trim_end) = hostname.split_at(1);
+    let output = common::render_module("hostname")
+        .env_clear()
+        .use_config(toml::toml! {
+            [hostname]
+            ssh_only = false
+            trim_end = trim_end
+        })
+        .output()?;
+    let actual = String::from_utf8(output.stdout).unwrap();
+    let expected = format!("on {} ", style().paint(remainder));
+    assert_eq!(expected, actual);
+    Ok(())
+}
+
 fn get_hostname() -> Option<String> {
     match gethostname::gethostname().into_string() {
         Ok(hostname) => Some(hostname),
