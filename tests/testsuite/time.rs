58493499b6b218db3115baafb609fe261cdf53e2
Zhenhui Xie <xiezh0831@yahoo.co.jp>
2019-10-05 09:45:12 +0800

    Remove disable configs from `testsuite/time.rs`

--- tests/testsuite/time.rs
+++ tests/testsuite/time.rs
@@ -11,13 +11,8 @@ should not display when disabled, should display *something* when enabled,
 and should have the correct prefixes and suffixes in a given config */
 
 #[test]
-fn config_enabled() -> io::Result<()> {
-    let output = common::render_module("time")
-        .use_config(toml::toml! {
-            [time]
-            disabled = false
-        })
-        .output()?;
+fn config_default() -> io::Result<()> {
+    let output = common::render_module("time").output()?;
     let actual = String::from_utf8(output.stdout).unwrap();
 
     // We can't test what it actually is...but we can assert it's not blank
@@ -30,7 +25,6 @@ fn config_check_prefix_and_suffix() -> io::Result<()> {
     let output = common::render_module("time")
         .use_config(toml::toml! {
             [time]
-            disabled = false
             format = "[%T]"
         })
         .output()?;
