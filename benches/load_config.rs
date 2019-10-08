052a792bdeb0d35e4e0afbca2e89e1191e806eeb
heyrict <xiezh0831@yahoo.co.jp>
2019-10-08 19:36:00 +0800

    Add benchmark for module config

---
+++ benches/load_config.rs
@@ -0,0 +1,71 @@
+#![feature(test)]
+
+extern crate starship;
+extern crate test;
+
+#[cfg(test)]
+mod tests {
+    use ansi_term::Color;
+    use starship::config::*;
+    use starship_module_config_derive::ModuleConfig;
+    use test::Bencher;
+
+    #[bench]
+    fn bench_load_config(b: &mut Bencher) {
+        #[derive(Clone, ModuleConfig)]
+        struct TestConfig<'a> {
+            pub prefix: &'a str,
+            pub disabled: bool,
+            pub some_array: Vec<&'a str>,
+        }
+
+        impl<'a> RootModuleConfig<'a> for TestConfig<'a> {
+            fn new() -> Self {
+                TestConfig {
+                    prefix: "on ",
+                    disabled: false,
+                    some_array: vec!["A", "B", "C"],
+                }
+            }
+        }
+
+        let config = toml::toml! {
+            prefix = "T "
+            disabled = true
+            some_array = ["A"]
+        };
+
+        b.iter(|| TestConfig::load(&config));
+    }
+
+    #[bench]
+    fn bench_load_segment_config(b: &mut Bencher) {
+        #[derive(Clone, ModuleConfig)]
+        struct TestConfig<'a> {
+            pub untracked: SegmentConfig<'a>,
+            pub modified: SegmentConfig<'a>,
+        }
+
+        impl<'a> RootModuleConfig<'a> for TestConfig<'a> {
+            fn new() -> Self {
+                TestConfig {
+                    untracked: SegmentConfig {
+                        value: "?",
+                        style: Some(Color::Red.bold()),
+                    },
+                    modified: SegmentConfig {
+                        value: "!",
+                        style: Some(Color::Red.bold()),
+                    },
+                }
+            }
+        }
+
+        let config = toml::toml! {
+            untracked.value = "x"
+            modified = { value = "•", style = "red" }
+        };
+
+        b.iter(|| TestConfig::load(&config))
+    }
+}
