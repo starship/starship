ee764cacf19fe0fd107552552b64b8dcd371a86c
Cédric Da Fonseca <dafonseca.cedric@gmail.com>
2019-10-18 11:53:30 +0200

    fix: aws: add ability to chose which item to display between region or profile (#539)

--- src/configs/aws.rs
+++ src/configs/aws.rs
@@ -1,7 +1,21 @@
 use crate::config::{ModuleConfig, RootModuleConfig, SegmentConfig};
 
+use toml::Value;
 use ansi_term::{Color, Style};
 use starship_module_config_derive::ModuleConfig;
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum AwsItems {
+    All,
+    Region,
+    Profile,
+}
+
+impl std::fmt::Display for AwsItems {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
 
 #[derive(Clone, ModuleConfig)]
 pub struct AwsConfig<'a> {
@@ -10,6 +24,7 @@ pub struct AwsConfig<'a> {
     pub region: SegmentConfig<'a>,
     pub style: Style,
     pub disabled: bool,
+    pub displayed_items: AwsItems,
 }
 
 impl<'a> RootModuleConfig<'a> for AwsConfig<'a> {
@@ -20,6 +35,18 @@ impl<'a> RootModuleConfig<'a> for AwsConfig<'a> {
             region: SegmentConfig::default(),
             style: Color::Yellow.bold(),
             disabled: false,
+            displayed_items: AwsItems::All,
+        }
+    }
+}
+
+impl<'a> ModuleConfig<'a> for AwsItems {
+    fn from_config(config: &Value) -> Option<AwsItems> {
+        match config {
+            Value::String(s) if s == "All" => Some(AwsItems::All),
+            Value::String(s) if s == "Region" => Some(AwsItems::Region),
+            Value::String(s) if s == "Profile" => Some(AwsItems::Profile),
+            _ => None,
         }
     }
 }
