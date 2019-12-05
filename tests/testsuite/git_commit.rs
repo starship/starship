8eb053b6d3053064198da475166010a5c02a5e31
Qingping Hou <qph@scribd.com>
2019-12-04 22:57:03 -0800

    add more test

--- tests/testsuite/git_commit.rs
+++ tests/testsuite/git_commit.rs
@@ -34,3 +34,35 @@ fn test_render_commit_hash() -> io::Result<()> {
     assert_eq!(&*expected, actual);
     Ok(())
 }
+
+#[test]
+fn test_render_commit_hash_len_override() -> io::Result<()> {
+    let repo_dir = common::create_fixture_repo()?;
+
+    let mut git_output = Command::new("git")
+        .args(&["rev-parse", "HEAD"])
+        .current_dir(repo_dir.as_path())
+        .output()?
+        .stdout;
+    git_output.truncate(14);
+    let expected_hash = str::from_utf8(&git_output).unwrap();
+
+    let output = common::render_module("git_commit")
+        .use_config(toml::toml! {
+            [git_commit]
+                disabled = false
+                commit_hash_length = 14
+        })
+        .arg("--path")
+        .arg(repo_dir)
+        .output()?;
+
+    let actual = String::from_utf8(output.stdout).unwrap();
+    let expected = Color::Green
+        .bold()
+        .paint(format!("({}) ", expected_hash))
+        .to_string();
+
+    assert_eq!(&*expected, actual);
+    Ok(())
+}
