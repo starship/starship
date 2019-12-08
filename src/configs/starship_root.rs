ad629e26c80e59d490b03f4d96a5e21439412b59
heyrict <xiezh0831@yahoo.co.jp>
2019-12-08 15:52:45 +0800

    fix: Remove unexpected spaces between modules

--- src/configs/starship_root.rs
+++ src/configs/starship_root.rs
@@ -16,11 +16,11 @@ impl<'a> RootModuleConfig<'a> for StarshipRootConfig<'a> {
             // List of default prompt order
             // NOTE: If this const value is changed then Default prompt order subheading inside
             // prompt heading of config docs needs to be updated according to changes made here.
-            prompt_order: "${username}${hostname}${kubernetes}${directory} \
-                           ${git_branch}${git_commit}${git_state}${git_status}${hg_branch} \
-                           ${package}${dotnet}${golang}${java} \
-                           ${nodejs}${python}${ruby}${rust} \
-                           ${nix_shell}${conda}${memory_usage}${aws} \
+            prompt_order: "${username}${hostname}${kubernetes}${directory}\
+                           ${git_branch}${git_commit}${git_state}${git_status}${hg_branch}\
+                           ${package}${dotnet}${golang}${java}\
+                           ${nodejs}${python}${ruby}${rust}\
+                           ${nix_shell}${conda}${memory_usage}${aws}\
                            ${env_var}${cmd_duration}\n\
                            ${jobs}${battery}${time}${character}",
             scan_timeout: 30,
