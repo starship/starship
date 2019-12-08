fc16dc8731454e9033c5f8bcc7e9e13b44c554b2
heyrict <xiezh0831@yahoo.co.jp>
2019-12-08 09:35:25 +0800

    refactor: Refactor php to use format string

--- src/configs/php.rs
+++ src/configs/php.rs
@@ -1,20 +1,17 @@
-use crate::config::{ModuleConfig, RootModuleConfig, SegmentConfig};
+use crate::config::{ModuleConfig, RootModuleConfig};
 
-use ansi_term::{Color, Style};
 use starship_module_config_derive::ModuleConfig;
 
 #[derive(Clone, ModuleConfig)]
 pub struct PhpConfig<'a> {
-    pub symbol: SegmentConfig<'a>,
-    pub style: Style,
+    pub format: &'a str,
     pub disabled: bool,
 }
 
 impl<'a> RootModuleConfig<'a> for PhpConfig<'a> {
     fn new() -> Self {
         PhpConfig {
-            symbol: SegmentConfig::new("🐘 "),
-            style: Color::Red.bold(),
+            format: "${styled?value=🐘 &style=red bold} ",
             disabled: false,
         }
     }
--- src/modules/php.rs
+++ src/modules/php.rs
@@ -1,8 +1,10 @@
 use std::process::Command;
 
-use super::{Context, Module, RootModuleConfig, SegmentConfig};
+use super::utils::query_parser::*;
+use super::{Context, Module, RootModuleConfig};
 
 use crate::configs::php::PhpConfig;
+use crate::segment::Segment;
 
 /// Creates a module with the current PHP version
 ///
@@ -25,11 +27,22 @@ pub fn module<'a>(context: &'a Context) -> Option<Module<'a>> {
             let mut module = context.new_module("php");
             let config: PhpConfig = PhpConfig::try_load(module.config);
 
-            module.set_style(config.style);
-
             let formatted_version = format_php_version(&php_version)?;
-            module.create_segment("symbol", &config.symbol);
-            module.create_segment("version", &SegmentConfig::new(&formatted_version));
+
+            let segments: Vec<Segment> = format_segments(config.format, None, |name, query| {
+                let style = get_style_from_query(&query);
+                match name {
+                    "version" => Some(Segment {
+                        _name: "version".to_string(),
+                        value: formatted_version.to_string(),
+                        style,
+                    }),
+                    _ => None,
+                }
+            })
+            .ok()?;
+
+            module.set_segments(segments);
 
             Some(module)
         }
