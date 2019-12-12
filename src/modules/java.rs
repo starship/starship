fc61284420303c0023d6e47d792fbdbca8178c64
Mario Arias <mario@marioarias.org>
2019-12-12 09:15:50 +0800

    Add 'build.gradle.kts' for Java detection

--- src/modules/java.rs
+++ src/modules/java.rs
@@ -9,11 +9,11 @@ use crate::utils;
 ///
 /// Will display the Java version if any of the following criteria are met:
 ///     - Current directory contains a file with a `.java`, `.class` or `.jar` extension
-///     - Current directory contains a `pom.xml`, `build.gradle` or `build.sbt` file
+///     - Current directory contains a `pom.xml`, `build.gradle`, `build.gradle.kts` or `build.sbt` file
 pub fn module<'a>(context: &'a Context) -> Option<Module<'a>> {
     let is_java_project = context
         .try_begin_scan()?
-        .set_files(&["pom.xml", "build.gradle", "build.sbt"])
+        .set_files(&["pom.xml", "build.gradle", "build.gradle.kts", "build.sbt"])
         .set_extensions(&["java", "class", "jar"])
         .is_match();
 
