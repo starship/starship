b6fe4bc5f9cb115c6b8290ad067e0c7e4e1bbdd6
Jon Grythe Stødle <jonstodle@outlook.com>
2019-12-19 20:58:05 +0100

    Insert line_break instead of prevent modules from printing

--- src/print.rs
+++ src/print.rs
@@ -59,21 +59,23 @@ pub fn get_prompt(context: Context) -> String {
 
     for module in printable {
         // Skip printing the prefix of a module after the line_break
-        let len = if print_without_prefix {
+        segments_len += if print_without_prefix {
             segments_len = 0;
             module.segments_len_without_prefix()
         } else {
             module.segments_len()
         };
 
-        if max_len == 0 || module.get_name() == "line_break" || segments_len + len < max_len - 1 {
-            if print_without_prefix {
-                write!(buf, "{}", module.to_string_without_prefix()).unwrap();
-            } else {
-                write!(buf, "{}", module).unwrap();
-            }
+        if max_len > 0 && segments_len > max_len - 1 {
+            write!(buf, "{}", modules::handle("line_break", &context).unwrap()).unwrap();
+            segments_len = module.segments_len_without_prefix();
+            print_without_prefix = true;
+        }
 
-            segments_len += len;
+        if print_without_prefix {
+            write!(buf, "{}", module.to_string_without_prefix()).unwrap();
+        } else {
+            write!(buf, "{}", module).unwrap();
         }
 
         print_without_prefix = module.get_name() == "line_break"
