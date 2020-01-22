ad99dd64bc999bdf6402e3250b047a983acc1ec3
Matan Kushner <hello@matchai.dev>
2020-01-22 13:02:06 -0500

    Make tests run with unknown shell

--- src/modules/utils/test.rs
+++ src/modules/utils/test.rs
@@ -1,9 +1,12 @@
 use std::path::Path;
+use crate::context::{Context, Shell};
+use crate::config::StarshipConfig;
 
 /// Render a specific starship module by name
 pub fn render_module(module_name: &str, path: &Path) -> Option<String> {
-    let mut context = crate::context::Context::new_with_dir(clap::ArgMatches::default(), path);
-    context.config = crate::config::StarshipConfig { config: None };
+    let mut context = Context::new_with_dir(clap::ArgMatches::default(), path);
+    context.config = StarshipConfig { config: None };
+    context.shell = Shell::Unknown;
 
     crate::print::get_module(module_name, context)
 }
