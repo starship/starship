26a09cd5174adc333e9e10a28805adca8ec8350c
jnskw <jk@jonaskowalick.com>
2020-03-10 08:26:18 +0100

    Make git branch prefix configurable

--- src/configs/git_branch.rs
+++ src/configs/git_branch.rs
@@ -5,6 +5,7 @@ use starship_module_config_derive::ModuleConfig;
 
 #[derive(Clone, ModuleConfig)]
 pub struct GitBranchConfig<'a> {
+    pub prefix: &'a str,
     pub symbol: SegmentConfig<'a>,
     pub truncation_length: i64,
     pub truncation_symbol: &'a str,
@@ -16,6 +17,7 @@ pub struct GitBranchConfig<'a> {
 impl<'a> RootModuleConfig<'a> for GitBranchConfig<'a> {
     fn new() -> Self {
         GitBranchConfig {
+            prefix: "on ",
             symbol: SegmentConfig::new(" "),
             truncation_length: std::i64::MAX,
             truncation_symbol: "…",
