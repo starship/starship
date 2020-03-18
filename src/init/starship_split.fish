e151573b8d95eddc1e2db1b3e7fcb6629d77d958
ZigTag <17077422+ZigTag@users.noreply.github.com>
2020-03-18 13:45:02 -0700

    Fish would not desplay the time on the right prompt

--- src/init/starship_split.fish
+++ src/init/starship_split.fish
@@ -12,6 +12,15 @@ function fish_prompt
 end
 
 function fish_right_prompt
+    switch "$fish_key_bindings"
+        case fish_hybrid_key_bindings fish_vi_key_bindings
+            set keymap "$fish_bind_mode"
+        case '*'
+            set keymap insert
+    end
+    set -l exit_code $status
+    # Account for changes in variable name between v2.7 and v3.0
+    set -l starship_duration "$CMD_DURATION$cmd_duration"
     ::STARSHIP:: prompt --status=$exit_code --keymap=$keymap --cmd-duration=$starship_duration --jobs=(count (jobs -p))
 end
 
