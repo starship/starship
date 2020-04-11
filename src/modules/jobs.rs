2eaeccab38f0b5167458b509d2850b75e63822ee
John Letey <johnletey@gmail.com>
2020-04-11 08:56:27 +0100

    refactor(jobs): Use format strings

--- src/modules/jobs.rs
+++ src/modules/jobs.rs
@@ -1,14 +1,12 @@
-use super::{Context, Module};
+use super::{Context, Module, RootModuleConfig};
 
-use crate::config::{RootModuleConfig, SegmentConfig};
 use crate::configs::jobs::JobsConfig;
+use crate::formatter::StringFormatter;
 
 /// Creates a segment to show if there are any active jobs running
 pub fn module<'a>(context: &'a Context) -> Option<Module<'a>> {
     let mut module = context.new_module("jobs");
     let config: JobsConfig = JobsConfig::try_load(module.config);
-
-    module.set_style(config.style);
 
     let props = &context.properties;
     let num_of_jobs = props
@@ -20,11 +18,25 @@ pub fn module<'a>(context: &'a Context) -> Option<Module<'a>> {
     if num_of_jobs == 0 {
         return None;
     }
-    module.create_segment("symbol", &config.symbol);
+    let mut module_number = "".to_string();
     if num_of_jobs > config.threshold {
-        module.create_segment("number", &SegmentConfig::new(&num_of_jobs.to_string()));
+        module_number = num_of_jobs.to_string();
     }
+
+    let formatter = if let Ok(formatter) = StringFormatter::new(config.format) {
+        formatter.map(|variable| match variable {
+            "number" => Some(module_number.to_string()),
+            _ => None,
+        })
+    } else {
+        log::warn!("Error parsing format string in `jobs.format`");
+        return None;
+    };
+
+    module.set_segments(formatter.parse(None));
+
     module.get_prefix().set_value("");
+    module.get_suffix().set_value("");
 
     Some(module)
 }
