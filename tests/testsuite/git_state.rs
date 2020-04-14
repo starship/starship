e431737661b67cc62b855d595a3810afa2d2125f
Thomas O'Donnell <andy.tom@gmail.com>
2020-04-14 16:10:58 +0200

    Add null tests to modules that don't have them
    Have added some null tests to modules that don't already have them.

--- tests/testsuite/git_state.rs
+++ tests/testsuite/git_state.rs
@@ -6,6 +6,21 @@ use std::process::{Command, Stdio};
 use tempfile;
 
 #[test]
+fn show_nothing_on_empty_dir() -> io::Result<()> {
+    let repo_dir = tempfile::tempdir()?;
+
+    let output = common::render_module("git_state")
+        .arg("--path")
+        .arg(repo_dir.path())
+        .output()?;
+    let actual = String::from_utf8(output.stdout).unwrap();
+
+    let expected = "";
+    assert_eq!(expected, actual);
+    repo_dir.close()
+}
+
+#[test]
 #[ignore]
 fn shows_rebasing() -> io::Result<()> {
     let repo_dir = create_repo_with_conflict()?;
