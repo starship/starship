5dd7c9c85f1fb4a06e5451b0191cd17c52e39a87
John Letey <johnletey@gmail.com>
2020-04-18 11:48:52 +0100

    refactor(elm): Use format strings

--- src/configs/elm.rs
+++ src/configs/elm.rs
@@ -1,22 +1,17 @@
-use crate::config::{ModuleConfig, RootModuleConfig, SegmentConfig};
+use crate::config::{ModuleConfig, RootModuleConfig};
 
-use ansi_term::{Color, Style};
 use starship_module_config_derive::ModuleConfig;
 
 #[derive(Clone, ModuleConfig)]
 pub struct ElmConfig<'a> {
-    pub symbol: SegmentConfig<'a>,
-    pub version: SegmentConfig<'a>,
-    pub style: Style,
+    pub format: &'a str,
     pub disabled: bool,
 }
 
 impl<'a> RootModuleConfig<'a> for ElmConfig<'a> {
     fn new() -> Self {
         ElmConfig {
-            symbol: SegmentConfig::new("🌳 "),
-            version: SegmentConfig::default(),
-            style: Color::Cyan.bold(),
+            format: "via [🌳 $version](cyan bold) ",
             disabled: false,
         }
     }
--- src/modules/elm.rs
+++ src/modules/elm.rs
@@ -1,6 +1,7 @@
-use super::{Context, Module, RootModuleConfig, SegmentConfig};
+use super::{Context, Module, RootModuleConfig};
 
 use crate::configs::elm::ElmConfig;
+use crate::formatter::StringFormatter;
 use crate::utils;
 
 /// Creates a module with the current Elm version
@@ -24,15 +25,25 @@ pub fn module<'a>(context: &'a Context) -> Option<Module<'a>> {
     }
 
     let elm_version = utils::exec_cmd("elm", &["--version"])?.stdout;
-    let formatted_version = Some(format!("v{}", elm_version.trim()))?;
+    let module_version = Some(format!("v{}", elm_version.trim()))?;
 
     let mut module = context.new_module("elm");
     let config: ElmConfig = ElmConfig::try_load(module.config);
-    module.set_style(config.style);
+    let formatter = if let Ok(formatter) = StringFormatter::new(config.format) {
+        formatter.map(|variable| match variable {
+            "version" => Some(module_version.clone()),
+            _ => None,
+        })
+    } else {
+        log::warn!("Error parsing format string in `elm.format`");
+        return None;
+    };
 
-    module.create_segment("symbol", &config.symbol);
-    module.create_segment("version", &SegmentConfig::new(&formatted_version));
+    module.set_segments(formatter.parse(None));
 
+    module.get_prefix().set_value("");
+    module.get_suffix().set_value("");
+    
     Some(module)
 }
 
