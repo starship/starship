ab1c3d1c5431a4fbc9a7e2d268feb28c0c2b8e5f
Radu Butoi <rbutoi@users.noreply.github.com>
2020-05-31 13:32:35 -0400

    feat(directory): Add directory substitutions (#1183)
    Adds an option to provide a table of strings to substitute in the
    directory string. Fixes #1065.
    
    Co-authored-by: Radu Butoi <butoi@google.com>

--- src/configs/directory.rs
+++ src/configs/directory.rs
@@ -1,4 +1,5 @@
 use crate::config::{ModuleConfig, RootModuleConfig};
+use std::collections::HashMap;
 
 use ansi_term::{Color, Style};
 use starship_module_config_derive::ModuleConfig;
@@ -7,6 +8,7 @@ use starship_module_config_derive::ModuleConfig;
 pub struct DirectoryConfig<'a> {
     pub truncation_length: i64,
     pub truncate_to_repo: bool,
+    pub substitutions: HashMap<String, &'a str>,
     pub fish_style_pwd_dir_length: i64,
     pub use_logical_path: bool,
     pub prefix: &'a str,
@@ -20,6 +22,7 @@ impl<'a> RootModuleConfig<'a> for DirectoryConfig<'a> {
             truncation_length: 3,
             truncate_to_repo: true,
             fish_style_pwd_dir_length: 0,
+            substitutions: HashMap::new(),
             use_logical_path: true,
             prefix: "in ",
             style: Color::Cyan.bold(),
