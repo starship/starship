a7b165f5cd35ccea852870c9d2bcdd1517733acf
Alexey Chernyshov <eiden127@gmail.com>
2020-06-20 00:11:57 +0300

    use correct access masks for macOS

--- src/modules/utils/directory_nix.rs
+++ src/modules/utils/directory_nix.rs
@@ -1,13 +1,8 @@
 use nix::sys::stat::Mode;
-use nix::unistd;
 use nix::unistd::{Gid, Uid};
 use std::fs;
 use std::os::unix::fs::MetadataExt;
 use std::os::unix::fs::PermissionsExt;
-
-const USER_WRITE_BIT: u32 = Mode::S_IWUSR.bits();
-const GROUP_WRITE_BIT: u32 = Mode::S_IWGRP.bits();
-const OTHER_WRITE_BIT: u32 = Mode::S_IWOTH.bits();
 
 pub fn is_write_allowed(folder_path: &str) -> Result<bool, &'static str> {
     let meta = fs::metadata(folder_path).map_err(|_| "Unable to stat() directory")?;
@@ -18,19 +13,19 @@ pub fn is_write_allowed(folder_path: &str) -> Result<bool, &'static str> {
         return Ok(true);
     }
     if meta.uid() == euid.as_raw() {
-        Ok(perms & USER_WRITE_BIT != 0)
+        Ok(perms & Mode::S_IWUSR.bits() as u32 != 0)
     } else if (meta.gid() == Gid::effective().as_raw())
         || (get_supplementary_groups().contains(&meta.gid()))
     {
-        Ok(perms & GROUP_WRITE_BIT != 0)
+        Ok(perms & Mode::S_IWGRP.bits() as u32 != 0)
     } else {
-        Ok(perms & OTHER_WRITE_BIT != 0)
+        Ok(perms & Mode::S_IWOTH.bits() as u32 != 0)
     }
 }
 
 #[cfg(all(unix, not(target_os = "macos")))]
 fn get_supplementary_groups() -> Vec<u32> {
-    match unistd::getgroups() {
+    match nix::unistd::getgroups() {
         Err(_) => Vec::new(),
         Ok(v) => v.into_iter().map(|i| i.as_raw()).collect(),
     }
