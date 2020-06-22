9d025cbedc65c25fb950000cdc52cf0509d639cc
heyrict <xiezh0831@yahoo.co.jp>
2020-06-22 13:56:36 +0800

    fix: Run rustfmt

--- src/formatter/mod.rs
+++ src/formatter/mod.rs
@@ -2,5 +2,5 @@ pub mod model;
 mod parser;
 pub mod string_formatter;
 
-pub use model::{VariableHolder, StyleVariableHolder};
+pub use model::{StyleVariableHolder, VariableHolder};
 pub use string_formatter::StringFormatter;
