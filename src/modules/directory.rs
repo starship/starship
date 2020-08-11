51825c6290544c031ef4a73c6643af0bf7e00d34
David Knaack <davidkna@users.noreply.github.com>
2020-08-11 09:55:02 +0200

    fix(test/dir): make lock thread-safe

--- src/modules/directory.rs
+++ src/modules/directory.rs
@@ -430,8 +430,7 @@ mod tests {
         #[test]
         #[ignore]
         fn symlinked_subdirectory_git_repo_out_of_tree() -> io::Result<()> {
-            while LOCK.load(Ordering::Relaxed) {}
-            LOCK.store(true, Ordering::Relaxed);
+            while LOCK.swap(true, Ordering::Relaxed) {}
             let tmp_dir = TempDir::new_in(home_dir().unwrap().as_path())?;
             let repo_dir = tmp_dir.path().join("above-repo").join("rocket-controls");
             let src_dir = repo_dir.join("src/meters/fuel-gauge");
@@ -460,8 +459,7 @@ mod tests {
         #[test]
         #[ignore]
         fn git_repo_in_home_directory_truncate_to_repo_true() -> io::Result<()> {
-            while LOCK.load(Ordering::Relaxed) {}
-            LOCK.store(true, Ordering::Relaxed);
+            while LOCK.swap(true, Ordering::Relaxed) {}
             let tmp_dir = TempDir::new_in(home_dir().unwrap().as_path())?;
             let dir = tmp_dir.path().join("src/fuel-gauge");
             fs::create_dir_all(&dir)?;
