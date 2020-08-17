087a4aeee30ea54ad7725d3c6645f382923cb4a7
Matan Kushner <hello@matchai.dev>
2020-08-17 11:57:21 -0400

    Revert "fix: restore add_newline configuration (#1592)"
    This reverts commit 1652c0bdb1d3c62c78c4e8bdde146464b6331539.

--- src/modules/line_break.rs
+++ src/modules/line_break.rs
@@ -5,36 +5,9 @@ use crate::segment::Segment;
 pub fn module<'a>(context: &'a Context) -> Option<Module<'a>> {
     const LINE_ENDING: &str = "\n";
 
-    let show_newline = context.config.get_root_config().add_newline;
-    if !show_newline {
-        return None;
-    }
-
     let mut module = context.new_module("line_break");
 
     module.set_segments(vec![Segment::new(None, LINE_ENDING)]);
 
     Some(module)
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::test::ModuleRenderer;
-
-    #[test]
-    fn add_newline_by_default() {
-        let expected = Some(String::from("\n"));
-        let actual = ModuleRenderer::new("line_break").collect();
-        assert_eq!(expected, actual);
-    }
-
-    #[test]
-    fn dont_add_newline_when_disabled() {
-        let expected = None;
-        let config = toml::toml! {
-            add_newline = false
-        };
-        let actual = ModuleRenderer::new("line_break").config(config).collect();
-        assert_eq!(expected, actual);
-    }
 }
