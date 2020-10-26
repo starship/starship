dcb132278798a13898b73fde33b07cc511197902
Mick Hohmann <gt@m69.im.net>
2020-10-26 19:58:38 +0100

    docs: udpated the gallery component for more options
    * extended the array in prompts.js to include more key/value pairs for more extensibility
    * updated the gallery component to include the new features from the prompt.js

--- docs/.vuepress/components/Gallery.vue
+++ docs/.vuepress/components/Gallery.vue
@@ -1,11 +1,26 @@
 <template>
   <div>
     <div v-for="prompt in prompts" :key="prompt.githubId">
-      <h3><a :href="'https://github.com/' + prompt.githubId">{{ prompt.githubId }}</a></h3>
-      <p>
-        View <code>starship.toml</code> of below prompt on <a :href="'https://github.com/starship/starship/blob/master/docs/.vuepress/public/gallery/' + prompt.githubId + '/starship.toml'">GitHub</a>.
-      </p>
-      <img :src="'/gallery/' + prompt.githubId + '/prompt.png'" alt="Prompt Image"/>
+      <h3><a :href="'https://github.com/' + prompt.githubId">{{ prompt.displayName }}</a></h3>
+      <div v-for="preset in prompt.presets" :key="preset.id">
+        <h4>{{preset.name}}</h4>
+        <img :src="'/gallery/' + prompt.githubId + '/' + preset.id + '/prompt.png'" :alt="preset.name + 'Image'"/>
+        <p v-if="preset.info.length != 0">
+          {{preset.info}}
+        </p>
+        <p v-if="preset.external">
+          Download the <a :href="preset.external" download>starship.toml</a> (please <em>right click</em> on the link and use <em>"Save Link As .../Download Linked File As ..."</em>).
+        </p>
+        <p v-else>
+          Download the <a :href="'/gallery/' + prompt.githubId +  '/' + preset.id + '/starship.toml'">starship.toml</a>.
+        </p>
+      </div>
+      <div v-if="prompt.presets.length == 0">
+        <img :src="'/gallery/' + prompt.githubId + '/prompt.png'" alt="Prompt Image"/>
+        <p>
+          Download the <a :href="'/gallery/' + prompt.githubId + '/starship.toml'">starship.toml</a>.
+        </p>
+      </div>
     </div>
   </div>
 </template>
