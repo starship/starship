344033663834fb1dac1126ea78f057579478d5c5
nukopy <pytwbf201830@gmail.com>
2020-11-02 01:12:30 +0900

    feat(docs): create a component for hovering image

--- docs/.vuepress/components/Gallery.vue
+++ docs/.vuepress/components/Gallery.vue
@@ -2,19 +2,13 @@
 <div>
     <div v-for="prompt in prompts" :key="prompt.id">
         <h2>{{ prompt.name  }}</h2>
-        <img :src="'/gallery/' + prompt.id + '.png'" :alt="prompt.name + ' Image'" @mouseover="hover = true" @mouseleave="hover = false" :class="{active: hover}" />
-        <p>{{ prompt.description }}</p>
-        <p>
-            Created by <a :href="'https://github.com/' + prompt.githubId">{{ prompt.displayCreatorName }}</a>.
-            Download the <a :href="prompt.link_raw" download>starship.toml</a> or view it on <a :href="prompt.link">GitHub</a>.
-        </p>
-        <!-- (please <em>right click</em> on the link and use <em>"Save Link As .../Download Linked File As ..."</em>). -->
+        <HoverImage :prompt="prompt" />
     </div>
 </div>
 </template>
 
 <script>
-// import HoverImage from "../components/HoverImage";
+import HoverImage from "../components/HoverImage";
 import prompts from "../public/gallery.json";
 
 export default {
@@ -22,7 +16,6 @@ export default {
     data() {
         return {
             prompts: prompts,
-            hover: false,
         }
     }
 }
---
+++ docs/.vuepress/components/HoverImage.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+    <img :src="'/gallery/' + prompt.id + '.png'" :alt="prompt.name + ' Image'" @mouseover="hover = true" @mouseleave="hover = false" :class="{active: hover}" />
+    <p v-show="hover">Mouceovered!!!</p>
+</div>
+</template>
+
+<script>
+export default {
+    props: ["prompt"],
+    data() {
+        return {
+            hover: false,
+        }
+    }
+}
+</script>
+
+<style scoped>
+.active {
+    border: 2px solid red;
+}
+</style>
