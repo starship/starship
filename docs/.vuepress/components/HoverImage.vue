d9f8a168889e4310ba65d1b0ebf04870acb0674c
nukopy <pytwbf201830@gmail.com>
2020-11-02 05:15:09 +0900

    feat(docs): add animation to images of the theme

--- docs/.vuepress/components/HoverImage.vue
+++ docs/.vuepress/components/HoverImage.vue
@@ -1,7 +1,18 @@
 <template>
 <div>
-    <img :src="'/gallery/' + prompt.id + '.png'" :alt="prompt.name + ' Image'" @mouseover="hover = true" @mouseleave="hover = false" :class="{active: hover}" />
-    <p v-show="hover">Mouceovered!!!</p>
+    <div @mouseover="hover=true" @mouseleave="hover = false" class="prompt">
+        <img class="image" :src="'/gallery/' + prompt.id + '.png'" :alt="prompt.name + ' Image'" />
+        <transition name="buttons">
+            <div class="buttons" v-if="hover">
+                <a :href="prompt.link_raw" class="action-button gallery-button" download>
+                    Download the starship.toml
+                </a>
+                <a :href="prompt.link" target="_black" class="action-button gallery-button">
+                    View on GitHub
+                </a>
+            </div>
+        </transition>
+    </div>
 </div>
 </template>
 
@@ -14,10 +25,69 @@ export default {
         }
     }
 }
+
 </script>
 
 <style scoped>
-.active {
-    border: 2px solid red;
+/* prompt */ 
+.prompt {
+    position: relative;  /* for putting buttons on image */
+}
+
+.prompt img {
+    transition: 0.75s all;
+}
+
+.prompt:hover img {
+    filter: brightness(60%);
+}
+
+/* buttons */ 
+.buttons {
+    position: absolute;  /* for putting buttons on image */
+    right: 1.5rem;
+    bottom: 2rem;
+    opacity: 0.9;
+}
+
+.buttons a {
+    color: white;
+    font-size: 1rem;
+    font-weight: 500;
+    margin-left: 0.5rem;
+}
+
+.buttons a:hover {
+    text-decoration: none;
+    position: relative;
+}
+
+.gallery-button {
+    border-style: none;
+    border-radius: 4px;
+    padding: 0.3rem 0.6rem;
+    cursor: pointer;
+}
+
+/* transition of "buttons" */
+.buttons-enter-active {
+    transition: all 0.5s ease;
+}
+
+.buttons-leave-active {
+    transition: all 0.5s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+    /* transition: all 0.75s cubic-bezier(1.0, 0.5, 0.8, 1.0);*/
+}
+
+.buttons-enter, .buttons-leave-to {
+    transform: translateY(5px);
+    opacity: 0;
+}
+
+@media (max-width: 480px) {
+    .buttons a {
+        display: block;
+        margin-top: 0.5rem;
+    }
 }
 </style>
