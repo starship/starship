9804288bb247897f2bb54b8887ffeb3ce5f4d9ab
Glandos <bugs-github@antipoul.fr>
2020-11-21 21:38:27 +0100

    Update src/init/starship.fish

--- src/init/starship.fish
+++ src/init/starship.fish
@@ -18,4 +18,4 @@ function fish_mode_prompt; end
 export STARSHIP_SHELL="fish"
 
 # Set up the session key that will be used to store logs
-export STARSHIP_SESSION_KEY=(string sub -s1 -l16 (random)(random)(random)(random)(random)0000000000000000)
+export STARSHIP_SESSION_KEY=(random 10000000000000 9999999999999999)
