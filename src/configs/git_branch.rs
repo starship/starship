4b85b40cb0af9e8c5eecdcf8931073f3c9e8779d
Aaron Gable <aaron@aarongable.com>
2020-11-23 10:22:51 -0800

    feat(git_branch): add 'only_attached' config bool (#1910)
    * feat(git_branch): add 'only_attached' config bool
    
    This adds a new boolean, `only_attached`, to the configuration for
    the `git_branch` module. This mirrors the `only_detached` config
    value for the `git_commit` module: setting the value to true causes
    the module to suppress its output when the user is not on a branch.
    
    This allows users to have either a branch name or a commit hash in
    their prompt, as opposed to having either a branch name or the
    overly-wordy "HEAD (sha1)".
    
    * Fix formatting

--- src/configs/git_branch.rs
+++ src/configs/git_branch.rs
@@ -9,6 +9,7 @@ pub struct GitBranchConfig<'a> {
     pub style: &'a str,
     pub truncation_length: i64,
     pub truncation_symbol: &'a str,
+    pub only_attached: bool,
     pub disabled: bool,
 }
 
@@ -20,6 +21,7 @@ impl<'a> RootModuleConfig<'a> for GitBranchConfig<'a> {
             style: "bold purple",
             truncation_length: std::i64::MAX,
             truncation_symbol: "…",
+            only_attached: false,
             disabled: false,
         }
     }
