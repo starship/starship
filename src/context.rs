ff633d04d28a1f5d95eb0b52e60a6d709ce3ef3a
Ben Fox <deadalus.ai@gmail.com>
2021-01-07 19:42:39 +1300

    fix(context): Fix small regression in --path prefix stripping

--- src/context.rs
+++ src/context.rs
@@ -79,10 +79,11 @@ impl<'a> Context<'a> {
         // TODO: Currently gets the physical directory. Get the logical directory.
         let mut current_dir = Context::expand_tilde(dir.into());
 
-        // Handle Powershell prepending "Microsoft.PowerShell.Core\FileSystem::" to some paths
+        // Try and handle Powershell prepending "Microsoft.PowerShell.Core\FileSystem::" to some paths
         if shell == Shell::PowerShell {
-            if let Ok(no_prefix) =
-                current_dir.strip_prefix(r"Microsoft.PowerShell.Core\FileSystem::")
+            if let Some(no_prefix) = current_dir
+                .to_str()
+                .and_then(|s| s.strip_prefix(r"Microsoft.PowerShell.Core\FileSystem::"))
             {
                 current_dir = PathBuf::from(no_prefix);
             }
@@ -513,7 +514,7 @@ mod tests {
         // Test with powershell env variable
         env::set_var("STARSHIP_SHELL", "powershell");
         let context = Context::new_with_dir(clap::ArgMatches::default(), path_with_prefix.clone());
-        assert_eq!(context.current_dir, Path::new(r"path"));
+        assert_eq!(context.current_dir, Path::new(r"/path"));
 
         // Test with non-powershell env variable
         env::set_var("STARSHIP_SHELL", "bash");
