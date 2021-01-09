7fb650051ea14b54a23b389b144bd5b207d51b68
Kevin Song <chips@ksong.dev>
2021-01-09 05:04:52 +0000

    Add comments to the nixshell config

--- src/configs/nix_shell.rs
+++ src/configs/nix_shell.rs
@@ -16,6 +16,9 @@ impl<'a> RootModuleConfig<'a> for NixShellConfig<'a> {
     fn new() -> Self {
         NixShellConfig {
             format: "via [$symbol$state( \\($name\\))]($style) ",
+            /* Two spaces are needed to avoid issues with multiwidth emoji
+            support. Please do not change unless you can show that your 
+            change does not affect emoji rendering on affected shells. */
             symbol: "❄️  ",
             style: "bold blue",
             impure_msg: "impure",
