fe6f9eeb4de899b813b6c42a60597a0b86429557
Shu Kutsuzawa <cappyzawa@gmail.com>
2021-02-22 03:53:10 +0900

    feat(purescript): Configure when the module is shown (#2357)
    This makes it possible to configure when the purescript module is shown
    based on the contents of a directory. This should make it possible to
    be a lot more granular when configuring the module.

--- src/configs/purescript.rs
+++ src/configs/purescript.rs
@@ -8,6 +8,9 @@ pub struct PureScriptConfig<'a> {
     pub symbol: &'a str,
     pub style: &'a str,
     pub disabled: bool,
+    pub detect_extensions: Vec<&'a str>,
+    pub detect_files: Vec<&'a str>,
+    pub detect_folders: Vec<&'a str>,
 }
 
 impl<'a> RootModuleConfig<'a> for PureScriptConfig<'a> {
@@ -17,6 +20,9 @@ impl<'a> RootModuleConfig<'a> for PureScriptConfig<'a> {
             symbol: "<=> ",
             style: "bold white",
             disabled: false,
+            detect_extensions: vec!["purs"],
+            detect_files: vec!["spago.dhall"],
+            detect_folders: vec![],
         }
     }
 }
