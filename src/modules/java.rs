fd47430d34569775eda5f91b25107cabe1a1089f
Thomas O'Donnell <andy.tom@gmail.com>
2021-03-12 14:51:14 +0100

    test(java): add test for JAVA_HOME

--- src/modules/java.rs
+++ src/modules/java.rs
@@ -88,6 +88,7 @@ mod tests {
     use ansi_term::Color;
     use std::fs::File;
     use std::io;
+    use std::path::PathBuf;
 
     #[test]
     fn test_parse_java_version_openjdk() {
@@ -264,6 +265,27 @@ mod tests {
         File::create(dir.path().join(".java-version"))?.sync_all()?;
         let actual = ModuleRenderer::new("java").path(dir.path()).collect();
         let expected = Some(format!("via {}", Color::Red.dimmed().paint("☕ v13.0.2 ")));
+        assert_eq!(expected, actual);
+        dir.close()
+    }
+
+    #[test]
+    fn test_java_home() -> io::Result<()> {
+        let dir = tempfile::tempdir()?;
+        File::create(dir.path().join("Main.java"))?.sync_all()?;
+        let java_home: PathBuf = ["a", "b", "c"].iter().collect();
+        let java_bin = java_home.join("bin").join("java");
+
+        let actual = ModuleRenderer::new("java")
+            .env("JAVA_HOME", java_home.to_str().unwrap())
+            .cmd(&format!("{} -Xinternalversion", java_bin.to_str().unwrap()),
+            Some(CommandOutput {
+                stdout: "OpenJDK 64-Bit Server VM (11.0.4+11-LTS-sapmachine) for linux-amd64 JRE (11.0.4+11-LTS-sapmachine), built on Jul 17 2019 08:58:43 by \"\" with gcc 7.3.0".to_owned(),
+                stderr: String::new(),
+            }))
+            .path(dir.path())
+            .collect();
+        let expected = Some(format!("via {}", Color::Red.dimmed().paint("☕ v11.0.4 ")));
         assert_eq!(expected, actual);
         dir.close()
     }
