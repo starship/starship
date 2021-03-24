34a07949a26770770505228113009257ee57e47f
Chris Rose <offline@offby1.net>
2021-03-24 10:14:04 -0700

    feat(python): add a configuration feature to format the version
    Define a new configuration value for python that formats the version. By
    default it is `v${major}.${minor}.${patch}`

--- src/modules/python.rs
+++ src/modules/python.rs
@@ -82,17 +82,43 @@ fn get_python_version(context: &Context, config: &PythonConfig) -> Option<String
             }
         })?;
 
-    format_python_version(&version)
+    format_python_version(config.version_format, &version)
 }
 
-fn format_python_version(python_version: &str) -> Option<String> {
+fn format_python_version(version_format: &str, python_version: &str) -> Option<String> {
     let version = python_version
         // split into ["Python", "3.8.6", ...]
         .split_whitespace()
-        // return "3.8.6"
-        .nth(1)?;
+        // get down to "3.8.6"
+        .nth(1)?
+        // split out major/minor/patch
+        .split('.')
+        // assemble into a 3-part vector
+        .collect::<Vec<&str>>();
 
-    Some(format!("v{}", version))
+    let parsed = StringFormatter::new(version_format).and_then(|formatter| {
+        formatter
+            .map(|variable| match variable {
+                "major" => version.get(0).map(|major| Ok(*major)),
+                "minor" => version.get(1).map(|major| Ok(*major)),
+                "patch" => version.get(2).map(|major| Ok(*major)),
+                _ => None,
+            })
+            .parse(None)
+    });
+    match parsed {
+        Ok(segments) => Some(
+            segments
+                .iter()
+                .map(|segment| segment.value.as_str())
+                .collect::<Vec<&str>>()
+                .join(""),
+        ),
+        Err(error) => {
+            log::warn!("Error in module `python` version format: \n{}", error);
+            None
+        }
+    }
 }
 
 fn get_python_virtual_env(context: &Context) -> Option<String> {
@@ -124,19 +150,46 @@ mod tests {
     #[test]
     fn test_format_python_version() {
         let input = "Python 3.7.2";
-        assert_eq!(format_python_version(input), Some("v3.7.2".to_string()));
+        assert_eq!(
+            format_python_version("v${major}.${minor}.${patch}", input),
+            Some("v3.7.2".to_string())
+        );
+    }
+
+    #[test]
+    fn test_format_python_version_truncated() {
+        let input = "Python 3.7.2";
+        assert_eq!(
+            format_python_version("v${major}.${minor}", input),
+            Some("v3.7".to_string())
+        );
+    }
+
+    #[test]
+    fn test_format_python_version_is_malformed() {
+        let input = "Python 3.7";
+        assert_eq!(
+            format_python_version("v${major}.${minor}.${patch}", input),
+            Some("v3.7.".to_string())
+        );
     }
 
     #[test]
     fn test_format_python_version_anaconda() {
         let input = "Python 3.6.10 :: Anaconda, Inc.";
-        assert_eq!(format_python_version(input), Some("v3.6.10".to_string()));
+        assert_eq!(
+            format_python_version("v${major}.${minor}.${patch}", input),
+            Some("v3.6.10".to_string())
+        );
     }
 
     #[test]
     fn test_format_python_version_pypy() {
         let input = "Python 3.7.9 (7e6e2bb30ac5fbdbd443619cae28c51d5c162a02, Nov 24 2020, 10:03:59)\n[PyPy 7.3.3-beta0 with GCC 10.2.0]";
-        assert_eq!(format_python_version(input), Some("v3.7.9".to_string()));
+        assert_eq!(
+            format_python_version("v${major}.${minor}.${patch}", input),
+            Some("v3.7.9".to_string())
+        );
     }
 
     #[test]
