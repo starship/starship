ed52cbfcc16fbafa01f038a96da835b0ca0d9986
Kid <44045911+kidonng@users.noreply.github.com>
2021-04-24 10:50:19 +0800

    Prefix variable names with Starship

--- src/init/starship.fish
+++ src/init/starship.fish
@@ -1,14 +1,14 @@
 function fish_prompt
     switch "$fish_key_bindings"
         case fish_hybrid_key_bindings fish_vi_key_bindings
-            set keymap "$fish_bind_mode"
+            set STARSHIP_KEYMAP "$fish_bind_mode"
         case '*'
-            set keymap insert
+            set STARSHIP_KEYMAP insert
     end
-    set exit_code $status
+    set STARSHIP_CMD_STATUS $status
     # Account for changes in variable name between v2.7 and v3.0
-    set starship_duration "$CMD_DURATION$cmd_duration"
-    ::STARSHIP:: prompt --status=$exit_code --keymap=$keymap --cmd-duration=$starship_duration --jobs=(count (jobs -p))
+    set STARSHIP_DURATION "$CMD_DURATION$cmd_duration"
+    ::STARSHIP:: prompt --status=$STARSHIP_CMD_STATUS --keymap=$STARSHIP_KEYMAP --cmd-duration=$STARSHIP_DURATION --jobs=(count (jobs -p))
 end
 
 # disable virtualenv prompt, it breaks starship
