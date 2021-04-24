00214d8407a898d61407ced922395b6243d17115
Kid <44045911+kidonng@users.noreply.github.com>
2021-04-24 10:57:39 +0800

    Use `functions -e` to remove `fish_mode_prompt`

--- src/init/starship.fish
+++ src/init/starship.fish
@@ -14,7 +14,9 @@ end
 # disable virtualenv prompt, it breaks starship
 set -g VIRTUAL_ENV_DISABLE_PROMPT 1
 
-function fish_mode_prompt; end
+# Remove default mode prompt
+builtin functions -e fish_mode_prompt
+
 set -gx STARSHIP_SHELL "fish"
 
 # Set up the session key that will be used to store logs
