7e0f58cbb0b89f781c9646a41c67d5ec9426881d
Filip Bachul <filipbachul@gmail.com>
2021-05-24 21:14:43 +0200

    java - consistent parse version method names & small refactor

--- src/modules/java.rs
+++ src/modules/java.rs
@@ -34,7 +34,11 @@ pub fn module<'a>(context: &'a Context) -> Option<Module<'a>> {
                 _ => None,
             })
             .map(|variable| match variable {
-                "version" => get_java_version(context, &config).map(Ok),
+                "version" => {
+                    let java_version_string = get_java_version(context)?;
+                    format_java_version(module.get_name(), &java_version_string, config.version_format)
+                        .map(Ok)
+                }
                 _ => None,
             })
             .parse(None)
@@ -51,7 +55,7 @@ pub fn module<'a>(context: &'a Context) -> Option<Module<'a>> {
     Some(module)
 }
 
-fn get_java_version(context: &Context, config: &JavaConfig) -> Option<String> {
+fn get_java_version(context: &Context) -> Option<String> {
     let java_command = context
         .get_env("JAVA_HOME")
         .map(PathBuf::from)
@@ -65,27 +69,24 @@ fn get_java_version(context: &Context, config: &JavaConfig) -> Option<String> {
         .unwrap_or_else(|| String::from("java"));
 
     let output = context.exec_cmd(&java_command, &["-Xinternalversion"])?;
-    let java_version = if output.stdout.is_empty() {
+    let java_version_string = if output.stdout.is_empty() {
         output.stderr
     } else {
         output.stdout
     };
-
-    format_java_version(&java_version, config.version_format)
+    Some(java_version_string)
 }
 
-fn format_java_version(java_version: &str, version_format: &str) -> Option<String> {
+fn format_java_version(
+    module_name: &str,
+    java_version_string: &str,
+    version_format: &str,
+) -> Option<String> {
     let re = Regex::new(JAVA_VERSION_PATTERN).ok()?;
-    let captures = re.captures(java_version)?;
+    let captures = re.captures(java_version_string)?;
     let version = &captures["version"];
 
-    match VersionFormatter::format_version(version, version_format) {
-        Ok(formatted) => Some(formatted),
-        Err(error) => {
-            log::warn!("Error formatting `java` version:\n{}", error);
-            Some(format!("v{}", version))
-        }
-    }
+    VersionFormatter::format_module_version(module_name, version, version_format)
 }
 
 #[cfg(test)]
@@ -101,11 +102,11 @@ mod tests {
         let java_8 = "OpenJDK 64-Bit Server VM (25.222-b10) for linux-amd64 JRE (1.8.0_222-b10), built on Jul 11 2019 10:18:43 by \"openjdk\" with gcc 4.4.7 20120313 (Red Hat 4.4.7-23)";
         let java_11 = "OpenJDK 64-Bit Server VM (11.0.4+11-post-Ubuntu-1ubuntu219.04) for linux-amd64 JRE (11.0.4+11-post-Ubuntu-1ubuntu219.04), built on Jul 18 2019 18:21:46 by \"build\" with gcc 8.3.0";
         assert_eq!(
-            format_java_version(java_11, "v${raw}"),
+            format_java_version("java", java_11, "v${raw}"),
             Some("v11.0.4".to_string())
         );
         assert_eq!(
-            format_java_version(java_8, "v${raw}"),
+            format_java_version("java", java_8, "v${raw}"),
             Some("v1.8.0".to_string())
         );
     }
@@ -114,7 +115,7 @@ mod tests {
     fn test_format_java_version_oracle() {
         let java_8 = "Java HotSpot(TM) Client VM (25.65-b01) for linux-arm-vfp-hflt JRE (1.8.0_65-b17), built on Oct  6 2015 16:19:04 by \"java_re\" with gcc 4.7.2 20120910 (prerelease)";
         assert_eq!(
-            format_java_version(java_8, "v${raw}"),
+            format_java_version("java", java_8, "v${raw}"),
             Some("v1.8.0".to_string())
         );
     }
@@ -124,11 +125,11 @@ mod tests {
         let java_8 = "OpenJDK 64-Bit Server VM (25.222-b10) for linux-amd64 JRE (1.8.0_222-b10), built on Jul 11 2019 20:48:53 by \"root\" with gcc 7.3.1 20180303 (Red Hat 7.3.1-5)";
         let java_12 = "OpenJDK 64-Bit Server VM (12.0.2+10) for linux-amd64 JRE (12.0.2+10), built on Jul 18 2019 14:41:47 by \"jenkins\" with gcc 7.3.1 20180303 (Red Hat 7.3.1-5)";
         assert_eq!(
-            format_java_version(java_8, "v${raw}"),
+            format_java_version("java", java_8, "v${raw}"),
             Some("v1.8.0".to_string())
         );
         assert_eq!(
-            format_java_version(java_12, "v${raw}"),
+            format_java_version("java", java_12, "v${raw}"),
             Some("v12.0.2".to_string())
         );
     }
@@ -138,11 +139,11 @@ mod tests {
         let java_8 = "OpenJDK 64-Bit Server VM (25.222-b10) for linux-amd64 JRE (Zulu 8.40.0.25-CA-linux64) (1.8.0_222-b10), built on Jul 11 2019 11:36:39 by \"zulu_re\" with gcc 4.4.7 20120313 (Red Hat 4.4.7-3)";
         let java_11 = "OpenJDK 64-Bit Server VM (11.0.4+11-LTS) for linux-amd64 JRE (Zulu11.33+15-CA) (11.0.4+11-LTS), built on Jul 11 2019 21:37:17 by \"zulu_re\" with gcc 4.9.2 20150212 (Red Hat 4.9.2-6)";
         assert_eq!(
-            format_java_version(java_8, "v${raw}"),
+            format_java_version("java", java_8, "v${raw}"),
             Some("v1.8.0".to_string())
         );
         assert_eq!(
-            format_java_version(java_11, "v${raw}"),
+            format_java_version("java", java_11, "v${raw}"),
             Some("v11.0.4".to_string())
         );
     }
@@ -152,11 +153,11 @@ mod tests {
         let java_8 = "Eclipse OpenJ9 OpenJDK 64-bit Server VM (1.8.0_222-b10) from linux-amd64 JRE with Extensions for OpenJDK for Eclipse OpenJ9 8.0.222.0, built on Jul 17 2019 21:29:18 by jenkins with g++ (GCC) 7.3.1 20180303 (Red Hat 7.3.1-5)";
         let java_11 = "Eclipse OpenJ9 OpenJDK 64-bit Server VM (11.0.4+11) from linux-amd64 JRE with Extensions for OpenJDK for Eclipse OpenJ9 11.0.4.0, built on Jul 17 2019 21:51:37 by jenkins with g++ (GCC) 7.3.1 20180303 (Red Hat 7.3.1-5)";
         assert_eq!(
-            format_java_version(java_8, "v${raw}"),
+            format_java_version("java", java_8, "v${raw}"),
             Some("v1.8.0".to_string())
         );
         assert_eq!(
-            format_java_version(java_11, "v${raw}"),
+            format_java_version("java", java_11, "v${raw}"),
             Some("v11.0.4".to_string())
         );
     }
@@ -165,7 +166,7 @@ mod tests {
     fn test_format_java_version_graalvm() {
         let java_8 = "OpenJDK 64-Bit GraalVM CE 19.2.0.1 (25.222-b08-jvmci-19.2-b02) for linux-amd64 JRE (8u222), built on Jul 19 2019 17:37:13 by \"buildslave\" with gcc 7.3.0";
         assert_eq!(
-            format_java_version(java_8, "v${raw}"),
+            format_java_version("java", java_8, "v${raw}"),
             Some("v8".to_string())
         );
     }
@@ -175,11 +176,11 @@ mod tests {
         let java_8 = "OpenJDK 64-Bit Server VM (25.222-b10) for linux-amd64 JRE (1.8.0_222-b10), built on Jul 11 2019 20:48:53 by \"root\" with gcc 7.3.1 20180303 (Red Hat 7.3.1-5)";
         let java_11 = "OpenJDK 64-Bit Server VM (11.0.4+11-LTS) for linux-amd64 JRE (11.0.4+11-LTS), built on Jul 11 2019 20:06:11 by \"\" with gcc 7.3.1 20180303 (Red Hat 7.3.1-5)";
         assert_eq!(
-            format_java_version(java_8, "v${raw}"),
+            format_java_version("java", java_8, "v${raw}"),
             Some("v1.8.0".to_string())
         );
         assert_eq!(
-            format_java_version(java_11, "v${raw}"),
+            format_java_version("java", java_11, "v${raw}"),
             Some("v11.0.4".to_string())
         );
     }
@@ -188,7 +189,7 @@ mod tests {
     fn test_format_java_version_sapmachine() {
         let java_11 = "OpenJDK 64-Bit Server VM (11.0.4+11-LTS-sapmachine) for linux-amd64 JRE (11.0.4+11-LTS-sapmachine), built on Jul 17 2019 08:58:43 by \"\" with gcc 7.3.0";
         assert_eq!(
-            format_java_version(java_11, "v${raw}"),
+            format_java_version("java", java_11, "v${raw}"),
             Some("v11.0.4".to_string())
         );
     }
@@ -196,7 +197,7 @@ mod tests {
     #[test]
     fn test_format_java_version_unknown() {
         let unknown_jre = "Unknown JRE";
-        assert_eq!(format_java_version(unknown_jre, "v${raw}"), None);
+        assert_eq!(format_java_version("java", unknown_jre, "v${raw}"), None);
     }
 
     #[test]
