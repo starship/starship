fca265916b3a55a4ee204d841cf27d9d3c62d602
Chrissy LeMaire <potatoqualitee@users.noreply.github.com>
2021-07-09 11:49:20 +0200

    Remove usage of Get-Error

--- src/init/starship.ps1
+++ src/init/starship.ps1
@@ -82,7 +82,7 @@ function global:prompt {
         # In case we have a False on the Dollar hook, we know there's an error.
         if (-not $origDollarQuestion) {
             # We retrieve the InvocationInfo from the most recent error.
-            $lastCmdletError = try { Get-Error |  Where-Object { $_ -ne $null } | Select-Object -expand InvocationInfo } catch { $null }
+            $lastCmdletError = try { $error[0] |  Where-Object { $_ -ne $null } | Select-Object -ExpandProperty InvocationInfo } catch { $null }
             # We check if the last command executed matches the line that caused the last error, in which case we know
             # it was an internal Powershell command, otherwise, there MUST be an error code.
             $lastExitCodeForPrompt = if ($null -ne $lastCmdletError -and $lastCmd.CommandLine -eq $lastCmdletError.Line) { 1 } else { $origLastExitCode }
