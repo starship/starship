59a908f34fa559c84f47984717694a717cf91d83
Bill Risher <bill@encephaloinvestments.com>
2021-08-18 10:20:50 -0500

    added command faking for COBOL

--- src/utils.rs
+++ src/utils.rs
@@ -85,6 +85,19 @@ pub fn exec_cmd(cmd: &str, args: &[&str], time_limit: Duration) -> Option<Comman
         _ => format!("{} {}", cmd, args.join(" ")),
     };
     match command.as_str() {
+        "cobc -version" => Some(CommandOutput {
+            stdout: String::from("\
+cobc (GnuCOBOL) 3.1.2.0
+Copyright (C) 2020 Free Software Foundation, Inc.
+License GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>
+This is free software; see the source for copying conditions.  There is NO
+warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+Written by Keisuke Nishida, Roger While, Ron Norman, Simon Sobisch, Edward Hart
+Built     Dec 24 2020 19:08:58
+Packaged  Dec 23 2020 12:04:58 UTC
+C version \"10.2.0\""),
+            stderr: String::default(),
+        }),
         "crystal --version" => Some(CommandOutput {
             stdout: String::from(
                 "\
