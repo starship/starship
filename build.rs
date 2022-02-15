2127b11e78c65011e4ff1cea24e717fde63c7802
Segev Finer <segev208@gmail.com>
2022-02-15 18:01:05 +0200

    Switch to winres crate

--- build.rs
+++ build.rs
@@ -1,5 +1,13 @@
-fn main() -> shadow_rs::SdResult<()> {
-    shadow_rs::new()?;
-    embed_resource::compile("starship-res.rc");
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    shadow_rs::new().map_err(|err| err.to_string())?;
+
+    if cfg!(target_os = "windows") {
+        let mut res = winres::WindowsResource::new();
+        res.set_manifest_file("starship.exe.manifest");
+        res.compile()?;
+    }
+
     Ok(())
 }
