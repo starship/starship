4360e7092d43a0da87a831234422f61126cdd17f
ksqsf <i@ksqsf.moe>
2022-03-16 17:46:58 +0800

    fix(haskell): fix tests and add a real stack.yaml testcase

--- src/modules/haskell.rs
+++ src/modules/haskell.rs
@@ -116,6 +116,7 @@ mod tests {
                 "resolver:\n  url: https://github.com/whatever/xxx.yaml\n",
                 "<custom snapshot>",
             ),
+            (REANIMATE_STACK_YAML, "lts-14.27"),
         ];
         for (yaml, resolver) in &cases {
             let dir = tempfile::tempdir()?;
@@ -125,7 +126,7 @@ mod tests {
             let actual = ModuleRenderer::new("haskell").path(dir.path()).collect();
             let expected = Some(format!(
                 "via {}",
-                Color::Purple.bold().paint(format!(" {} ", resolver))
+                Color::Purple.bold().paint(format!("λ {} ", resolver))
             ));
             assert_eq!(expected, actual);
             dir.close()?;
@@ -140,10 +141,34 @@ mod tests {
             let dir = tempfile::tempdir()?;
             File::create(dir.path().join(hs_file))?.sync_all()?;
             let actual = ModuleRenderer::new("haskell").path(dir.path()).collect();
-            let expected = Some(format!("via {}", Color::Purple.bold().paint(" 9.2.1 ")));
+            let expected = Some(format!("via {}", Color::Purple.bold().paint("λ 9.2.1 ")));
             assert_eq!(expected, actual);
             dir.close()?;
         }
         Ok(())
     }
+
+    static REANIMATE_STACK_YAML: &'static str = r"
+resolver: lts-14.27
+
+allow-newer: false
+
+packages:
+- .
+
+extra-deps:
+- reanimate-svg-0.13.0.1
+- chiphunk-0.1.2.1
+- cubicbezier-0.6.0.6@sha256:2191ff47144d9a13a2784651a33d340cd31be1926a6c188925143103eb3c8db3
+- fast-math-1.0.2@sha256:91181eb836e54413cc5a841e797c42b2264954e893ea530b6fc4da0dccf6a8b7
+- matrices-0.5.0@sha256:b2761813f6a61c84224559619cc60a16a858ac671c8436bbac8ec89e85473058
+- hmatrix-0.20.0.0@sha256:d79a9218e314f1a2344457c3851bd1d2536518ecb5f1a2fcd81daa45e46cd025,4870
+- earcut-0.1.0.4@sha256:d5118b3eecf24d130263d81fb30f1ff56b1db43036582bfd1d8cc9ba3adae8be,1010
+- tasty-rerun-1.1.17@sha256:d4a3ccb0f63f499f36edc71b33c0f91c850eddb22dd92b928aa33b8459f3734a,1373
+- hgeometry-0.11.0.0@sha256:09ead201a6ac3492c0be8dda5a6b32792b9ae87cab730b8362d46ee8d5c2acb4,11714
+- hgeometry-combinatorial-0.11.0.0@sha256:03176f235a1c49a415fe1266274dafca84deb917cbcbf9a654452686b4cd2bfe,8286
+- vinyl-0.13.0@sha256:0f247cd3f8682b30881a07de18e6fec52d540646fbcb328420049cc8d63cd407,3724
+- hashable-1.3.0.0@sha256:4c70f1407881059e93550d3742191254296b2737b793a742bd901348fb3e1fb1,5206
+- network-3.1.2.1@sha256:188d6daea8cd91bc3553efd5a90a1e7c6d0425fa66a53baa74db5b6d9fd75c8b,4968
+";
 }
