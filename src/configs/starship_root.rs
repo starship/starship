c152e984d945720ab95fda57af5b7f28faf76dbe
Kevin Song <chips@ksong.dev>
2022-04-15 23:19:21 -0500

    Format and update config-schema

--- src/configs/starship_root.rs
+++ src/configs/starship_root.rs
@@ -1,5 +1,5 @@
-use serde::{Deserialize, Serialize};
 use indexmap::IndexMap;
+use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Serialize, Deserialize, Debug)]
 #[cfg_attr(feature = "config-schema", derive(schemars::JsonSchema))]
