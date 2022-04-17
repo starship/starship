b9da4487daf525951a6f854fef53f2ca07e08559
DonizeteVida <donizetevida1998@outlook.com>
2022-04-17 18:50:06 -0300

    Elixir dynamic variable capability
    Add to elixir module the capability to resolve
    multiple versions' variables allowing user to
    write custom toml describing which variables
    to format and how to get them.

--- src/modules/elixir.rs
+++ src/modules/elixir.rs
@@ -5,7 +5,8 @@ use crate::formatter::StringFormatter;
 
 use crate::formatter::VersionFormatter;
 use once_cell::sync::Lazy;
-use std::ops::Deref;
+use regex::Regex;
+use std::collections::HashMap;
 
 /// Create a module with the current Elixir version
 pub fn module<'a>(context: &'a Context) -> Option<Module<'a>> {
@@ -23,7 +24,11 @@ pub fn module<'a>(context: &'a Context) -> Option<Module<'a>> {
         return None;
     }
 
-    let versions = Lazy::new(|| get_elixir_version(context));
+    let versions: Lazy<String, _> = Lazy::new(|| {
+        get_elixir_versions(context, config.version_command).unwrap_or_else(|| "".to_string())
+    });
+    let extracted_versions: Lazy<HashMap<&str, &str>, _> =
+        Lazy::new(|| parse_elixir_versions(&versions, config.version_extract_map));
 
     let parsed = StringFormatter::new(config.format).and_then(|formatter| {
         formatter
@@ -35,25 +40,15 @@ pub fn module<'a>(context: &'a Context) -> Option<Module<'a>> {
                 "style" => Some(Ok(config.style)),
                 _ => None,
             })
-            .map(|variable| match variable {
-                "version" => versions
-                    .deref()
-                    .as_ref()
-                    .map(|(_, elixir_version)| elixir_version)
-                    .map(|elixir_version| {
-                        VersionFormatter::format_module_version(
-                            module.get_name(),
-                            elixir_version,
-                            config.version_format,
-                        )
-                    })?
-                    .map(Ok),
-                "otp_version" => versions
-                    .deref()
-                    .as_ref()
-                    .map(|(otp_version, _)| otp_version.to_string())
-                    .map(Ok),
-                _ => None,
+            .map(|variable| match extracted_versions.get(variable) {
+                Some(version) => match config.version_format_map.get(variable) {
+                    Some(format) => {
+                        VersionFormatter::format_module_version(module.get_name(), version, format)
+                            .map(Ok)
+                    }
+                    None => Some(Ok(version.to_string())),
+                },
+                None => None,
             })
             .parse(None, Some(context))
     });
@@ -69,22 +64,25 @@ pub fn module<'a>(context: &'a Context) -> Option<Module<'a>> {
     Some(module)
 }
 
-fn get_elixir_version(context: &Context) -> Option<(String, String)> {
-    let output = context.exec_cmd("elixir", &["--version"])?.stdout;
-
-    parse_elixir_version(&output)
+fn get_elixir_versions(context: &Context, version_command: &str) -> Option<String> {
+    Some(context.exec_cmd("elixir", &[version_command])?.stdout)
 }
 
-fn parse_elixir_version(version: &str) -> Option<(String, String)> {
-    let mut lines = version.lines();
-    // split line into ["Erlang/OTP", "22", "[erts-10.5]", ...], take "22"
-    let otp_version = lines.next()?.split_whitespace().nth(1)?;
-    // skip empty line
-    let _ = lines.next()?;
-    // split line into ["Elixir", "1.10", "(compiled", ...], take "1.10"
-    let elixir_version = lines.next()?.split_whitespace().nth(1)?;
-
-    Some((otp_version.to_string(), elixir_version.to_string()))
+fn parse_elixir_versions<'a>(
+    versions: &'a str,
+    version_extract_map: HashMap<&'a str, &'a str>,
+) -> HashMap<&'a str, &'a str> {
+    let mut extracted_versions = HashMap::new();
+    for (key, value) in version_extract_map.into_iter() {
+        let regex = Regex::new(value).unwrap();
+        if let Some(ref captures) = regex.captures(versions) {
+            let option_option_match = captures.iter().nth(1);
+            if let Some(Some(m)) = option_option_match {
+                extracted_versions.insert(key, m.as_str());
+            }
+        }
+    }
+    extracted_versions
 }
 
 #[cfg(test)]
@@ -113,16 +111,36 @@ Erlang/OTP 23 [erts-11.1.7] [source] [64-bit] [smp:8:8] [ds:8:8:10] [async-threa
 Elixir 1.13.0-dev (compiled with Erlang/OTP 23)
 ";
         assert_eq!(
-            parse_elixir_version(stable_input),
-            Some(("23".to_string(), "1.11.3".to_string()))
+            parse_elixir_versions(
+                stable_input,
+                HashMap::from([
+                    ("version", "Elixir\\s([\\d\\.\\-\\w]+)"),
+                    ("otp_version", "Erlang/OTP\\s([\\d\\.\\-\\w]+)"),
+                ])
+            ),
+            HashMap::from([("version", "1.11.3"), ("otp_version", "23")])
         );
+
         assert_eq!(
-            parse_elixir_version(rc_input),
-            Some(("23".to_string(), "1.12.0-rc.0".to_string()))
+            parse_elixir_versions(
+                rc_input,
+                HashMap::from([
+                    ("version", "Elixir\\s([\\d\\.\\-\\w]+)"),
+                    ("otp_version", "Erlang/OTP\\s([\\d\\.\\-\\w]+)"),
+                ])
+            ),
+            HashMap::from([("version", "1.12.0-rc.0"), ("otp_version", "23")])
         );
+
         assert_eq!(
-            parse_elixir_version(dev_input),
-            Some(("23".to_string(), "1.13.0-dev".to_string()))
+            parse_elixir_versions(
+                dev_input,
+                HashMap::from([
+                    ("version", "Elixir\\s([\\d\\.\\-\\w]+)"),
+                    ("otp_version", "Erlang/OTP\\s([\\d\\.\\-\\w]+)"),
+                ])
+            ),
+            HashMap::from([("version", "1.13.0-dev"), ("otp_version", "23")])
         );
     }
 
