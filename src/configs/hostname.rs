2bf30dc89fbce6f4da37657b8af6077f15a543d0
Thanapat Chotipun <66824385+PatrickChoDev@users.noreply.github.com>
2022-05-04 20:30:16 +0700

    feat(hostname): add `ssh_symbol` for ssh connections (#3806)

--- src/configs/hostname.rs
+++ src/configs/hostname.rs
@@ -5,6 +5,7 @@ use serde::{Deserialize, Serialize};
 #[serde(default)]
 pub struct HostnameConfig<'a> {
     pub ssh_only: bool,
+    pub ssh_symbol: &'a str,
     pub trim_at: &'a str,
     pub format: &'a str,
     pub style: &'a str,
@@ -15,8 +16,9 @@ impl<'a> Default for HostnameConfig<'a> {
     fn default() -> Self {
         HostnameConfig {
             ssh_only: true,
+            ssh_symbol: "🌐 ",
             trim_at: ".",
-            format: "[$hostname]($style) in ",
+            format: "[$ssh_symbol$hostname]($style) in ",
             style: "green dimmed bold",
             disabled: false,
         }
