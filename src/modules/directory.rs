7ce6e1f93c7ddec39cadf4f39f06e53d7a3a5b83
Filip Bachul <filipbachul@gmail.com>
2022-07-07 19:03:36 +0200

    update contract_windows_style_root_directory test

--- src/modules/directory.rs
+++ src/modules/directory.rs
@@ -429,7 +429,7 @@ mod tests {
         let top_level_path = Path::new("C:\\Users\\astronaut");
 
         let output = contract_path(full_path, top_level_path, "~");
-        assert_eq!(output, "C:");
+        assert_eq!(output, "C:/");
     }
 
     #[test]
