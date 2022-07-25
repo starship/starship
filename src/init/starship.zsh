f793b9f77347fdda15804349de3542f6a595d4b3
Rashil Gandhi <46838874+rashil2000@users.noreply.github.com>
2022-07-26 00:39:05 +0530

    feat(zsh): Enable left and right transience

--- src/init/starship.zsh
+++ src/init/starship.zsh
@@ -62,6 +62,66 @@ starship_zle-keymap-select() {
     zle reset-prompt
 }
 
+_starship-zle-line-init() {
+    [[ $CONTEXT == start ]] || return 0
+
+    # Start regular line editor
+    (( $+zle_bracketed_paste )) && print -r -n - $zle_bracketed_paste[1]
+    zle .recursive-edit
+    local -i ret=$?
+    (( $+zle_bracketed_paste )) && print -r -n - $zle_bracketed_paste[2]
+
+    is_prompt_transient=true
+    zle .reset-prompt
+    is_prompt_transient=false
+
+    # If we received EOT, we exit the shell
+    if [[ $ret == 0 && $KEYS == $'\4' ]]; then
+        exit
+    fi
+
+    # Ctrl-C
+    if (( ret )); then
+        zle .send-break
+    else
+        # Enter
+        zle .accept-line
+    fi
+    return ret
+}
+
+function enable_transience {
+  zle -N zle-line-init _starship-zle-line-init
+}
+
+function disable_transience {
+  zle -D zle-line-init
+}
+
+function starship_left_prompt {
+    if [ "$is_prompt_transient" = true ] ; then
+        if whence -w starship_transient_prompt_func >/dev/null; then
+            starship_transient_prompt_func
+        else
+            echo "\e[1;32m❯\e[0m "
+        fi
+    else
+        ::STARSHIP:: prompt --terminal-width="$COLUMNS" --keymap="${KEYMAP:-}" --status="$STARSHIP_CMD_STATUS" --pipestatus="${STARSHIP_PIPE_STATUS[*]}" --cmd-duration="${STARSHIP_DURATION:-}" --jobs="$STARSHIP_JOBS_COUNT"
+    fi
+}
+
+function starship_right_prompt {
+    if [ "$is_prompt_transient" = true ] ; then
+        if whence -w starship_transient_rprompt_func >/dev/null; then
+            starship_transient_rprompt_func
+        else
+            echo ""
+        fi
+    else
+        ::STARSHIP:: prompt --right --terminal-width="$COLUMNS" --keymap="${KEYMAP:-}" --status="$STARSHIP_CMD_STATUS" --pipestatus="${STARSHIP_PIPE_STATUS[*]}" --cmd-duration="${STARSHIP_DURATION:-}" --jobs="$STARSHIP_JOBS_COUNT"
+    fi
+}
+
 ## Check for existing keymap-select widget.
 # zle-keymap-select is a special widget so it'll be "user:fnName" or nothing. Let's get fnName only.
 __starship_preserved_zle_keymap_select=${widgets[zle-keymap-select]#user:}
@@ -78,6 +138,8 @@ fi
 
 __starship_get_time && STARSHIP_START_TIME=$STARSHIP_CAPTURED_TIME
 
+is_prompt_transient=false
+
 export STARSHIP_SHELL="zsh"
 
 # Set up the session key that will be used to store logs
@@ -89,7 +151,7 @@ VIRTUAL_ENV_DISABLE_PROMPT=1
 
 setopt promptsubst
 
-PROMPT='$(::STARSHIP:: prompt --terminal-width="$COLUMNS" --keymap="${KEYMAP:-}" --status="$STARSHIP_CMD_STATUS" --pipestatus="${STARSHIP_PIPE_STATUS[*]}" --cmd-duration="${STARSHIP_DURATION:-}" --jobs="$STARSHIP_JOBS_COUNT")'
-RPROMPT='$(::STARSHIP:: prompt --right --terminal-width="$COLUMNS" --keymap="${KEYMAP:-}" --status="$STARSHIP_CMD_STATUS" --pipestatus="${STARSHIP_PIPE_STATUS[*]}" --cmd-duration="${STARSHIP_DURATION:-}" --jobs="$STARSHIP_JOBS_COUNT")'
+PROMPT='$(starship_left_prompt)'
+RPROMPT='$(starship_right_prompt)'
 PROMPT2="$(::STARSHIP:: prompt --continuation)"
 
