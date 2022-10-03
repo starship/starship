cd306fcae7c0cc515da233f591a9b73f24d0c65e
AlexHaxe <Alexander.Blum@gmail.com>
2022-10-03 20:02:27 +0200

    better version of detecting contents of .haxerc
    Co-authored-by: David Knaack <davidkna@users.noreply.github.com>

--- src/modules/haxe.rs
+++ src/modules/haxe.rs
@@ -63,11 +63,7 @@ pub fn module<'a>(context: &'a Context) -> Option<Module<'a>> {
 
 fn get_haxe_version(context: &Context) -> Option<String> {
     let file_contents = context.read_file_from_pwd(".haxerc");
-    if file_contents.is_some() {
-        let raw_json = match file_contents {
-            Some(val) => val,
-            None => panic!("failed to read contents of .haxerc!"),
-        };
+    if let Some(raw_json) = file_contents {
         let haxerc_json: json::Value = json::from_str(&raw_json).ok()?;
         let raw_version = haxerc_json.get("version")?.as_str()?;
         parse_haxe_version(raw_version)
