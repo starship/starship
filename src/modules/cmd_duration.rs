bd8371440dedfb8e8b984d1bb183a588f22dd2b1
gabrielvictorcf <gabrielvcf@outlook.com>
2022-10-26 13:05:33 -0300

    fix new tests to only compile on linux

--- src/modules/cmd_duration.rs
+++ src/modules/cmd_duration.rs
@@ -206,6 +206,7 @@ mod tests {
         assert_eq!(expected, actual);
     }
 
+    #[cfg(target_os = "linux")]
     #[test]
     fn duration_disabled_on_tty() {
         let actual = ModuleRenderer::new("cmd_duration")
@@ -217,6 +218,7 @@ mod tests {
         assert_eq!(expected, actual);
     }
 
+    #[cfg(target_os = "linux")]
     #[test]
     fn duration_disabled_on_tty_works_on_multiplexers() {
         let actual = ModuleRenderer::new("cmd_duration")
