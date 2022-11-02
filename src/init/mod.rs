be9dcc43d0f2e1b159a91d420d57068b3a377e66
Kevin Song <chips@ksong.dev>
2022-11-01 23:12:58 -0500

    Favor finding Starship on the PATH over its absolute path on disk. (#910)

--- src/init/mod.rs
+++ src/init/mod.rs
@@ -3,6 +3,8 @@ use std::ffi::OsStr;
 use std::path::{Path, PathBuf};
 use std::{env, io};
 
+use which::which;
+
 /* We use a two-phase init here: the first phase gives a simple command to the
 shell. This command evaluates a more complicated script using `source` and
 process substitution.
@@ -23,9 +25,25 @@ struct StarshipPath {
 }
 impl StarshipPath {
     fn init() -> io::Result<Self> {
-        Ok(Self {
-            native_path: env::current_exe()?,
-        })
+        let exe_name = option_env!("CARGO_PKG_NAME").unwrap_or("starship");
+
+        let native_path = match which(exe_name) {
+            Ok(path) => {
+                let current_exe = path
+                    .to_str()
+                    .ok_or_else(|| io::Error::new(io::ErrorKind::Other, "can't convert to str"))?
+                    .to_string();
+                current_exe
+            }
+            Err(error) => {
+                let current_exe = env::current_exe()?
+                    .to_str()
+                    .ok_or_else(|| io::Error::new(io::ErrorKind::Other, "can't convert to str"))?
+                    .to_string();
+                current_exe
+            }
+        };
+        Ok(Self { native_path })
     }
     fn str_path(&self) -> io::Result<&str> {
         let current_exe = self
