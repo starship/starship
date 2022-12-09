7341607c294a633477005d777bd03b18522aabf4
Dennis Waitong Lam <77250215+Funinja@users.noreply.github.com>
2022-12-09 04:41:59 -0500

    fix(buf): broken icon on windows 10 (#4689)
    Closes https://github.com/starship/starship/issues/4608

--- src/configs/buf.rs
+++ src/configs/buf.rs
@@ -23,7 +23,7 @@ impl<'a> Default for BufConfig<'a> {
         BufConfig {
             format: "with [$symbol($version )]($style)",
             version_format: "v${raw}",
-            symbol: "🦬 ",
+            symbol: "🐃 ",
             style: "bold blue",
             disabled: false,
             detect_extensions: vec![],
