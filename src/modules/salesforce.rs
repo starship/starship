2821ac9a33e097a1c2038cedfa62d80aa190eb81
Przemek <rawa.przemek@gmail.com>
2022-12-14 12:52:00 +0100

    add support for sf command configs
    Signed-off-by: Przemek <rawa.przemek@gmail.com>

--- src/modules/salesforce.rs
+++ src/modules/salesforce.rs
@@ -1,37 +1,13 @@
 use std::path::PathBuf;
-use crate::config::ModuleConfig;
+
 use serde::{Deserialize, Serialize};
+
+use crate::config::ModuleConfig;
 use crate::configs::salesforce::SalesforceConfig;
-use crate::formatter::{StringFormatter};
+use crate::formatter::StringFormatter;
 use crate::utils::read_file;
+
 use super::{Context, Module};
-
-
-#[derive(Clone, Deserialize, Serialize)]
-#[serde(default)]
-struct SfdxOrgCommandOutputResult {
-    pub alias: Option<String>,
-    pub username: Option<String>,
-}
-
-impl Default for SfdxOrgCommandOutputResult {
-    fn default() -> Self {
-        SfdxOrgCommandOutputResult { alias: None, username: None }
-    }
-}
-
-#[derive(Clone, Deserialize, Serialize)]
-#[serde(default)]
-struct SfdxOrgCommandOutput {
-    pub status: i64,
-    pub result: Option<SfdxOrgCommandOutputResult>,
-}
-
-impl Default for SfdxOrgCommandOutput {
-    fn default() -> Self {
-        SfdxOrgCommandOutput { status: 1, result: None }
-    }
-}
 
 pub fn module<'a>(context: &'a Context) -> Option<Module<'a>> {
     let mut module = context.new_module("salesforce");
@@ -39,7 +15,7 @@ pub fn module<'a>(context: &'a Context) -> Option<Module<'a>> {
 
     let repo = match context.get_repo() {
         Ok(r) => r,
-        Err(error) => return None
+        Err(_error) => return None,
     };
 
     let org_name_option = match config.use_sfdx {
@@ -47,14 +23,14 @@ pub fn module<'a>(context: &'a Context) -> Option<Module<'a>> {
         false => {
             let workdir = match &repo.workdir {
                 Some(w) => w,
-                None => return None
+                None => return None,
             };
-            get_org_name(&workdir)
+            get_org_name_from_config_files(&workdir)
         }
     };
     let org_name = match org_name_option {
         Some(o) => o,
-        None => return None
+        None => return None,
     };
     let parsed = StringFormatter::new(config.format).and_then(|formatter| {
         formatter
@@ -97,7 +73,9 @@ struct SfdxCommandResultResult {
 }
 
 fn get_org_name_from_command(context: &Context) -> Option<String> {
-    let output = context.exec_cmd("sfdx", &["force:org:display", "--json"])?.stdout;
+    let output = context
+        .exec_cmd("sfdx", &["force:org:display", "--json"])?
+        .stdout;
     let result: SfdxCommandResult = match serde_json::from_str(output.as_str()) {
         Ok(r) => r,
         Err(error) => {
@@ -109,24 +87,26 @@ fn get_org_name_from_command(context: &Context) -> Option<String> {
         Some(r) => {
             match r.alias {
                 Some(alias) => return Some(alias),
-                None => ()
+                None => (),
             };
             return match r.username {
                 Some(username) => Some(username),
-                None => None
+                None => None,
             };
         }
-        None => None
+        None => None,
     }
 }
 
-
-fn get_org_name(repo_path: &PathBuf) -> Option<String> {
+fn get_org_name_from_config_files(repo_path: &PathBuf) -> Option<String> {
+    let sf_configuration_file = repo_path.join(".sf").join("config.json");
+    if sf_configuration_file.exists() {
+        return get_org_name_from_sf_config_file(sf_configuration_file);
+    }
     let sfdx_configuration_file = repo_path.join(".sfdx").join("sfdx-config.json");
     if sfdx_configuration_file.exists() {
         return get_org_name_from_sfdx_config_file(sfdx_configuration_file);
     }
-    //TODO from sf config file
     return None;
 }
 
@@ -151,4 +131,28 @@ fn get_org_name_from_sfdx_config_file(sfdx_config_file: PathBuf) -> Option<Strin
         }
     };
     return result.defaultusername;
+}
+
+#[derive(Deserialize, Serialize)]
+struct SfConfig {
+    #[serde(alias = "target-org")]
+    target_org: Option<String>,
+}
+
+fn get_org_name_from_sf_config_file(sfdx_config_file: PathBuf) -> Option<String> {
+    let file_contents = match read_file(sfdx_config_file) {
+        Ok(f) => f,
+        Err(error) => {
+            log::warn!("Module salesforce failed to read sf config: {}", error);
+            return None;
+        }
+    };
+    let result: SfConfig = match serde_json::from_str(file_contents.as_str()) {
+        Ok(r) => r,
+        Err(error) => {
+            log::warn!("Module salesforce failed to parse sf output: {}", error);
+            return None;
+        }
+    };
+    return result.target_org;
 }
