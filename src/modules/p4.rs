2eb65f143c6967e0445392edca7adec548c46da1
Pierre-Jean MOREL <pj33720@gmail.com>
2023-01-23 10:07:58 -0500

    Basic print of changelist number user and client

--- src/modules/p4.rs
+++ src/modules/p4.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use super::{Context, Module};
 
 use crate::config::ModuleConfig;
@@ -8,13 +10,31 @@ use crate::formatter::StringFormatter;
 ///
 /// Will display the name of the current Perforce repository if one is active.
 pub fn module<'a>(context: &'a Context) -> Option<Module<'a>> {
-    let repo = context.get_env("VCSH_REPO_NAME").unwrap_or_default();
-    if repo.trim().is_empty() {
+    let mut module = context.new_module("p4");
+    let config: P4Config = P4Config::try_load(module.config);
+
+    if config.disabled {
         return None;
     }
 
-    let mut module = context.new_module("p4");
-    let config: P4Config = P4Config::try_load(module.config);
+    let user_cmd = context.exec_cmd("p4", &["info"]);
+    let info_map = match &user_cmd {
+        Some(output) => parse_key_values(&output.stdout),
+        None => return None
+    };
+
+    let user = info_map.get("User name").unwrap_or(&"");
+    let client = info_map.get("Client name").unwrap_or(&"unknown");
+
+    if user.is_empty() {
+        return None;
+    }
+
+    let changelist_cmd = context.exec_cmd("p4", &["changes", "-m1", "#have"]);
+    let changelist = match &changelist_cmd {
+        Some(output) => output.stdout.split(" ").nth(1).unwrap_or("?"),
+        None => ""
+    };
 
     let parsed = StringFormatter::new(config.format).and_then(|formatter| {
         formatter
@@ -27,7 +47,9 @@ pub fn module<'a>(context: &'a Context) -> Option<Module<'a>> {
                 _ => None,
             })
             .map(|variable| match variable {
-                "repo" => Some(Ok(&repo)),
+                "changelist" => Some(Ok(changelist)),
+                "user" => Some(Ok(user)),
+                "client" => Some(Ok(client)),
                 _ => None,
             })
             .parse(None, Some(context))
@@ -42,6 +64,24 @@ pub fn module<'a>(context: &'a Context) -> Option<Module<'a>> {
     });
 
     Some(module)
+}
+
+fn parse_key_values(output: &str) -> HashMap<&str, &str> {
+    let mut info_map = HashMap::new();
+    let pairs = output
+        .split("\n")
+        .map(|e| { e.split(":") });
+            
+    for mut pair in pairs {
+        let k = pair.nth(0).unwrap_or_default().trim();
+        let v = pair.nth(0).unwrap_or_default().trim();
+
+        if !k.is_empty() {
+            info_map.insert(k, v);
+        }
+    }
+
+    info_map
 }
 
 #[cfg(test)]
