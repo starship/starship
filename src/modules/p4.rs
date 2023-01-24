986b86c60c427799803de799218cc27e5fba6f46
Pierre-Jean MOREL <pj33720@gmail.com>
2023-01-24 14:30:10 -0500

    First basic test

--- src/modules/p4.rs
+++ src/modules/p4.rs
@@ -18,7 +18,7 @@ pub fn module<'a>(context: &'a Context) -> Option<Module<'a>> {
     }
 
     let info = get_p4_info(context)?;
- 
+
     if !context.current_dir.starts_with(info.client_root) {
         return None;
     }
@@ -58,13 +58,13 @@ pub fn module<'a>(context: &'a Context) -> Option<Module<'a>> {
 struct P4Info {
     user_name: String,
     client_name: String,
-    client_root: String
+    client_root: String,
 }
 
 fn is_p4_logged(context: &Context) -> bool {
     match context.exec_cmd("p4", &["login", "-s"]) {
         Some(_) => true,
-        None => false
+        None => false,
     }
 }
 
@@ -75,12 +75,17 @@ fn get_p4_info(context: &Context) -> Option<P4Info> {
     Some(P4Info {
         user_name: info_map.get("User name")?.to_string(),
         client_name: info_map.get("Client name")?.to_string(),
-        client_root: info_map.get("Client root")?.to_string()
+        client_root: info_map.get("Client root")?.to_string(),
     })
 }
 
 fn get_p4_last_changelist_number<'a>(context: &Context) -> Option<String> {
-    context.exec_cmd("p4", &["changes", "-m1", "#have"])?.stdout.split(" ").nth(1).and_then(|s| Some(s.to_string()))
+    context
+        .exec_cmd("p4", &["changes", "-m1", "#have"])?
+        .stdout
+        .split(" ")
+        .nth(1)
+        .and_then(|s| Some(s.to_string()))
 }
 
 fn parse_p4_info_output(output: &str) -> HashMap<&str, &str> {
@@ -100,11 +105,57 @@ fn parse_p4_info_output(output: &str) -> HashMap<&str, &str> {
 
 #[cfg(test)]
 mod tests {
-    use crate::test::ModuleRenderer;
     use nu_ansi_term::Color;
 
+    use super::P4Info;
+
+    use crate::test::ModuleRenderer;
+
     #[test]
-    fn empty_test() {
-        assert_eq!("expected", "expected");
+    fn logged_inside_p4_workspace() {
+        let actual = ModuleRenderer::new("p4")
+            .path(r"C:\Perforce\MyWorkspace\MyRepository")
+            .collect();
+        let expected = Some(format!(
+            "{} ",
+            Color::Blue.bold().paint("p4 human@MyWorkspace#176579")
+        ));
+
+        assert_eq!(expected, actual);
+    }
+
+    // #[test]
+    // fn not_logged_inside_p4_workspace() -> io::Result<()> {
+
+    // }
+
+    // #[test]
+    // fn logged_outside_p4_workspace() -> io::Result<()> {
+
+    // }
+
+    fn build_mock_p4_info_output(info: &P4Info) -> String {
+        format!(
+            r"\
+User name: {}
+Client name: {}
+Client host: MyPC
+Client root: {}
+Current directory: {}
+Peer address: 127.0.0.1:55855
+Client address: 127.0.0.1
+Server address: sc-helixa.test.com:1666
+Server root: C:\Program Files\Perforce\Server
+Server date: 2023/01/23 18:09:14 -0500 Eastern Standard Time
+Server uptime: 661:14:26
+Server version: P4D/NTX64/2020.1/1953492 (2020/04/24)
+Server encryption: encrypted
+Server cert expires: Jul  3 13:53:58 2024 GMT
+Server license: University of Test 1000 users (support ends 2023/02/15) (expires 2023/02/15) 
+Server license-ip: 127.0.0.1:1666
+Case Handling: insensitive
+",
+            info.user_name, info.client_name, info.client_root, info.current_directory
+        )
     }
 }
