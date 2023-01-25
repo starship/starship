e889abd1eddc5197d204e1f8717190686d7b9f52
Pierre-Jean MOREL <pj33720@gmail.com>
2023-01-24 20:33:28 -0500

    Added other unit tests

--- src/modules/p4.rs
+++ src/modules/p4.rs
@@ -101,12 +101,12 @@ fn parse_p4_info_output(output: &str) -> HashMap<&str, &str> {
 mod tests {
     use nu_ansi_term::Color;
 
-    use crate::test::ModuleRenderer;
+    use crate::{modules::p4::P4Info, test::ModuleRenderer, utils::CommandOutput};
 
     #[test]
-    fn logged_inside_p4_workspace() {
+    fn logged_deep_inside_p4_workspace() {
         let actual = ModuleRenderer::new("p4")
-            .path(r"C:\Perforce\MyWorkspace\MyRepository")
+            .path(r"C:\Perforce\MyWorkspace\MyRepository\subdir")
             .collect();
         let expected = Some(format!(
             "{} ",
@@ -116,38 +116,115 @@ mod tests {
         assert_eq!(expected, actual);
     }
 
-    // #[test]
-    // fn not_logged_inside_p4_workspace() -> io::Result<()> {
+    #[test]
+    fn logged_just_inside_p4_workspace() {
+        let actual = ModuleRenderer::new("p4")
+            .path(r"C:\Perforce\MyWorkspace\")
+            .collect();
+        let expected = Some(format!(
+            "{} ",
+            Color::Blue.bold().paint("p4 human@MyWorkspace#176579")
+        ));
 
-    // }
+        assert_eq!(expected, actual);
+    }
 
-    // #[test]
-    // fn logged_outside_p4_workspace() -> io::Result<()> {
+    #[test]
+    fn not_logged_inside_p4_workspace() {
+        let actual = ModuleRenderer::new("p4")
+            .cmd("p4 login -s", None)
+            .path(r"C:\Perforce\MyWorkspace\MyRepository")
+            .collect();
+        let expected = None;
 
-    // }
+        assert_eq!(expected, actual);
+    }
 
-    //     fn build_mock_p4_info_output(info: &P4Info) -> String {
-    //         format!(
-    //             r"\
-    // User name: {}
-    // Client name: {}
-    // Client host: MyPC
-    // Client root: {}
-    // Current directory: c:\Users\human
-    // Peer address: 127.0.0.1:55855
-    // Client address: 127.0.0.1
-    // Server address: sc-helixa.test.com:1666
-    // Server root: C:\Program Files\Perforce\Server
-    // Server date: 2023/01/23 18:09:14 -0500 Eastern Standard Time
-    // Server uptime: 661:14:26
-    // Server version: P4D/NTX64/2020.1/1953492 (2020/04/24)
-    // Server encryption: encrypted
-    // Server cert expires: Jul  3 13:53:58 2024 GMT
-    // Server license: University of Test 1000 users (support ends 2023/02/15) (expires 2023/02/15)
-    // Server license-ip: 127.0.0.1:1666
-    // Case Handling: insensitive
-    // ",
-    //             info.user_name, info.client_name, info.client_root
-    //         )
-    //     }
+    #[test]
+    fn logged_outside_p4_workspace() {
+        let actual = ModuleRenderer::new("p4")
+            .path(r"C:\outside\perforce")
+            .collect();
+        let expected = None;
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn good_p4_username_and_workspace() {
+        let user_name = "testusernamer";
+        let client_name = "Workspace";
+        let client_root = r"C:\Perforce\Workspace";
+
+        let info = P4Info {
+            user_name: String::from(user_name),
+            client_name: String::from(client_name),
+            client_root: String::from(client_root),
+        };
+
+        let actual = ModuleRenderer::new("p4")
+            .cmd("p4 info", build_mock_p4_info_output(&info))
+            .path(client_root)
+            .collect();
+        let expected = Some(format!(
+            "{} ",
+            Color::Blue
+                .bold()
+                .paint(format!("p4 {}@{}#176579", user_name, client_name))
+        ));
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn good_p4_changelist_number() {
+        let changelist = 12;
+        let actual = ModuleRenderer::new("p4")
+            .cmd("p4 changes -m1 #have", build_mock_p4_changes_output(changelist))
+            .path(r"C:\Perforce\MyWorkspace\MyRepository")
+            .collect();
+        let expected = Some(format!(
+            "{} ",
+            Color::Blue
+                .bold()
+                .paint(format!("p4 human@MyWorkspace#{}", changelist))
+        ));
+
+        assert_eq!(expected, actual);
+    }
+
+    fn build_mock_p4_changes_output(changelist: i32) -> Option<CommandOutput> {
+        Some(CommandOutput {
+            stdout: format!("Change {} on 2023/01/23 by human@MyWorkspace 'doing some work'\n", changelist),
+            stderr: String::default()
+        })
+    }
+
+    fn build_mock_p4_info_output(info: &P4Info) -> Option<CommandOutput> {
+        Some(CommandOutput {
+            stdout: format!(
+                r"\
+User name: {}
+Client name: {}
+Client host: MyPC
+Client root: {}
+Current directory: c:\Users\human
+Peer address: 127.0.0.1:55855
+Client address: 127.0.0.1
+Server address: sc-helixa.test.com:1666
+Server root: C:\Program Files\Perforce\Server
+Server date: 2023/01/23 18:09:14 -0500 Eastern Standard Time
+Server uptime: 661:14:26
+Server version: P4D/NTX64/2020.1/1953492 (2020/04/24)
+Server encryption: encrypted
+Server cert expires: Jul  3 13:53:58 2024 GMT
+Server license: University of Test 1000 users (support ends 2023/02/15) (expires 2023/02/15)
+Server license-ip: 127.0.0.1:1666
+Case Handling: insensitive
+",
+                info.user_name, info.client_name, info.client_root
+            ),
+            stderr: String::default(),
+        })
+    }
 }
