1d5970518425d4b91ea2a95eefed77f561027fcc
Zhizhen He <hezhizhen.yi@gmail.com>
2023-03-08 23:42:00 +0800

    docs(install): fix typo and update indentation and whitespaces (#4941)

--- install/macos_packages/common.sh
+++ install/macos_packages/common.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-error(){
-    echo "[ERROR]: $1"
-    exit 1
+error() {
+  echo "[ERROR]: $1"
+  exit 1
 }
 
-starship_version(){
+starship_version() {
   starship_program_file="$1"
   # Check if this is a relative path: if so, prepend './' to it
   if [ "$1" = "${1#/}" ]; then
     starship_program_file="./$starship_program_file"
   fi
-  if "$starship_program_file" -V 2>&1 > /dev/null; then
+  if "$starship_program_file" -V 2>&1 >/dev/null; then
     "$starship_program_file" -V | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+'
   else
     # try to get this information from Cargo.toml
