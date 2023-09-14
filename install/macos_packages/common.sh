6355b8acda9854ed4a7ee1cd19a209e09fb1f3bf
Kevin Song <chips@ksong.dev>
2023-09-14 13:43:10 -0500

    fix: Change how starship version is determined

--- install/macos_packages/common.sh
+++ install/macos_packages/common.sh
@@ -11,12 +11,20 @@ starship_version() {
   if [ "$1" = "${1#/}" ]; then
     starship_program_file="./$starship_program_file"
   fi
-  if "$starship_program_file" -V 2>&1 >/dev/null; then
+
+  # Try to get the version from three sources in the following order:
+  #  - the STARSHIP_VERSION envar (usually set by the CI)
+  #  - Running the binary file
+  #  - By cutting out the first version tag in Cargo.toml
+  # These get increasingly fragile as we go down the list---ideally CI should
+  # always run with STARSHIP_VERSION set to avoid issues in determining version.
+  if [ "$STARSHIP_VERSION" != "" ]; then
+    echo "$STARSHIP_VERSION"
+  elif "$starship_program_file" -V >/dev/null 2>&1; then
     "$starship_program_file" -V | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+'
   else
-    # try to get this information from Cargo.toml
     pushd "$(git rev-parse --show-toplevel)" || true
-    grep '^version = \"\(.*\)\"' Cargo.toml | cut -f 2 -d '"'
+    grep '^version = \"\(.*\)\"' Cargo.toml | head -n 1 | cut -f 2 -d '"'
     popd
   fi
 }
