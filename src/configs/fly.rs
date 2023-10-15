371add7475db7d483cfb6a9845d039cfc61fe03e
Sharun <sharunksplus@gmail.com>
2023-10-15 00:40:57 -0400

    update format

--- src/configs/fly.rs
+++ src/configs/fly.rs
@@ -23,7 +23,7 @@ impl<'a> Default for FlyConfig<'a> {
         FlyConfig {
             // $version is excluded by default as flyctl version command takes too long
             // tracked in https://github.com/superfly/flyctl/issues/2908
-            format: "via [$symbol$app $primary_region ]($style)",
+            format: "via [$symbol($app )($primary_region )]($style)",
             version_format: "v${raw}",
             symbol: "🎈 ",
             style: "purple bold",
