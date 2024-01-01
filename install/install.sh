2f5755a1c8d4676e108d5bd1e07d2e7720eb666e
Ketan Sonar <ketan.sonar.798@gmail.com>
2024-01-01 20:07:30 +0530

    allow user to do autocomplete while entering the path

--- install/install.sh
+++ install/install.sh
@@ -269,12 +269,11 @@ confirm() {
       exit 1
     fi
     if [ "$yn" != "y" ] && [ "$yn" != "yes" ]; then
-      printf "Enter the location to install to: "
       set +e
-      read -r bd </dev/tty
-      BIN_DIR=$bd
+      read -p "Enter the location to install to: " -e path
       rc=$?
       set -e
+      BIN_DIR=$path
       if [ $rc -ne 0 ]; then
           error "Error reading from prompt (please re-run with the '--yes' option)"
           exit 1
