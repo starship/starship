2f42166a703f22d1d9c071282ebc11a1e1b2a26b
Max Niederman <max@maxniederman.com>
2024-12-27 01:45:14 -0800

    feat(netns): get netns from ip-netns

--- src/modules/netns.rs
+++ src/modules/netns.rs
@@ -7,14 +7,48 @@ pub fn module<'a>(_context: &'a Context) -> Option<Module<'a>> {
 
 #[cfg(target_os = "linux")]
 pub fn module<'a>(context: &'a Context) -> Option<Module<'a>> {
-    use crate::{config::ModuleConfig, configs::netns::NetnsConfig};
+    use crate::{config::ModuleConfig, configs::netns::NetnsConfig, formatter::StringFormatter};
 
-    let module = context.new_module("netns");
+    fn netns_name(context: &Context) -> Option<String> {
+        context
+            .exec_cmd("ip", &["netns", "identify"])
+            .map(|output| output.stdout.trim().to_string())
+            .filter(|name| !name.is_empty())
+    }
+
+    let mut module = context.new_module("netns");
     let config = NetnsConfig::try_load(module.config);
 
     if config.disabled {
         return None;
     }
+
+    let netns_name = netns_name(context)?;
+
+    let parsed = StringFormatter::new(config.format).and_then(|formatter| {
+        formatter
+            .map_meta(|variable, _| match variable {
+                "symbol" => Some(config.symbol),
+                _ => None,
+            })
+            .map_style(|variable| match variable {
+                "style" => Some(Ok(config.style)),
+                _ => None,
+            })
+            .map(|variable| match variable {
+                "name" => Some(Ok(&netns_name)),
+                _ => None,
+            })
+            .parse(None, Some(context))
+    });
+
+    module.set_segments(match parsed {
+        Ok(segments) => segments,
+        Err(error) => {
+            log::warn!("Error in module `netns`: \n{}", error);
+            return None;
+        }
+    });
 
     Some(module)
 }
