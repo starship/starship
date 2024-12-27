047ba391b9e4ee4e686385dc3cb2fa85aae96523
Max Niederman <max@maxniederman.com>
2024-12-27 01:27:02 -0800

    feat(netns): add config for netns module

--- src/modules/netns.rs
+++ src/modules/netns.rs
@@ -7,5 +7,14 @@ pub fn module<'a>(_context: &'a Context) -> Option<Module<'a>> {
 
 #[cfg(target_os = "linux")]
 pub fn module<'a>(context: &'a Context) -> Option<Module<'a>> {
-    None
+    use crate::{config::ModuleConfig, configs::netns::NetnsConfig};
+
+    let module = context.new_module("netns");
+    let config = NetnsConfig::try_load(module.config);
+
+    if config.disabled {
+        return None;
+    }
+
+    Some(module)
 }
