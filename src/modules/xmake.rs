65595fbe8ea8b143fda821466e7a432b01b701fe
Martin Grigorov <martin-g@users.noreply.github.com>
2025-09-14 14:55:12 +0300

    fix: Check if XMakeConfig is disabled
    Do not load the module if XMakeConfig says it is disabled.

--- src/modules/xmake.rs
+++ src/modules/xmake.rs
@@ -8,6 +8,10 @@ pub fn module<'a>(context: &'a Context) -> Option<Module<'a>> {
     let mut module = context.new_module("xmake");
     let config = XMakeConfig::try_load(module.config);
 
+    if config.disabled {
+        return None;
+    }
+
     let is_xmake_project = context
         .try_begin_scan()?
         .set_files(&config.detect_files)
