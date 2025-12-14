a379c254ef3ee902406d507c14e76c2ff6a4b140
David Knaack <davidkna@users.noreply.github.com>
2025-12-14 11:53:16 +0100

    chore: bump msrv & handle clippy warnings

--- src/modules/hostname.rs
+++ src/modules/hostname.rs
@@ -28,11 +28,9 @@ pub fn module<'a>(context: &'a Context) -> Option<Module<'a>> {
         .inspect_err(|e| log::warn!("Failed to get hostname: {e}"))
         .ok()?;
 
-    //rustc doesn't let you do an "if" and an "if let" in the same if statement
-    // if this changes in the future this can become a lot cleaner
-    let mut host = if config.trim_at.is_empty() {
-        host.as_ref()
-    } else if let Some(index) = host.find(config.trim_at) {
+    let mut host = if !config.trim_at.is_empty()
+        && let Some(index) = host.find(config.trim_at)
+    {
         host.split_at(index).0
     } else {
         host.as_ref()
