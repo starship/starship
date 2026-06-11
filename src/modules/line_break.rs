c8c7957af3294f26c03fe4727de4122b5482167a
Marshall <marshalll.taylorrr@gmail.com>
2026-06-11 18:05:09 -0400

    feat: add max_width to line_break and respect config
    Adds max_width option to [line_break] that only inserts a newline when the used prompt exceeds max_width chars. Defaults to 0, preserving original behavior
    
    Also fixes the line_break module to actually load its config object (LineBreakConfig) like other modules do

--- src/modules/line_break.rs
+++ src/modules/line_break.rs
@@ -1,11 +1,20 @@
-use super::{Context, Module};
+use super::{Context, Module, ModuleConfig};
 use crate::segment::Segment;
+
+use crate::configs::line_break::LineBreakConfig;
 
 /// Creates a module for the line break
 pub fn module<'a>(context: &'a Context) -> Option<Module<'a>> {
     let mut module = context.new_module("line_break");
+    let config: LineBreakConfig = LineBreakConfig::try_load(module.config);
 
-    module.set_segments(vec![Segment::LineTerm]);
+    if config.disabled {
+        return None;
+    }
+
+    module.set_segments(vec![Segment::LineTerm {
+        max_width: config.max_width,
+    }]);
 
     Some(module)
 }
