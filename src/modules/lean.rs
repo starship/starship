e88605233aa2177c05549d90c804a60e3bf41bc9
jmarx <julius@marx-trier.de>
2026-06-28 16:11:30 +0200

    [Adding-lean] - Added Lean config and module - Added Lean in module, configs/mod, configs/starship_root and modules/mod

---
+++ src/modules/lean.rs
@@ -0,0 +1,122 @@
+use super::{Context, Module, ModuleConfig};
+
+use crate::configs::lean::LeanConfig;
+use crate::formatter::StringFormatter;
+use crate::formatter::VersionFormatter;
+
+/// Creates a module with the current Gleam version
+pub fn module<'a>(context: &'a Context) -> Option<Module<'a>> {
+    let mut module = context.new_module("lean");
+    let config = LeanConfig::try_load(module.config);
+
+    let is_lean_config = context
+        .try_begin_scan()?
+        .set_files(&config.detect_files)
+        .set_extensions(&config.detect_extensions)
+        .set_folders(&config.detect_folders)
+        .is_match();
+
+    if !is_lean_config {
+        return None;
+    }
+
+    let parsed = StringFormatter::new(config.format).and_then(|formatter| {
+        formatter
+            .map_meta(|var, _| match var {
+                "symbol" => Some(config.symbol),
+                _ => None,
+            })
+            .map_style(|variable| match variable {
+                "style" => Some(Ok(config.style)),
+                _ => None,
+            })
+            .map(|variable| match variable {
+                "version" => {
+                    let lean_version =
+                        parse_lean_version(&context.exec_cmd("lean", &["--version"])?.stdout)?;
+                    VersionFormatter::format_module_version(
+                        module.get_name(),
+                        &lean_version,
+                        config.version_format,
+                    )
+                    .map(Ok)
+                }
+                _ => None,
+            })
+            .parse(None, Some(context))
+    });
+
+    module.set_segments(match parsed {
+        Ok(segments) => segments,
+        Err(error) => {
+            log::warn!("Error in module `lean`:\n{error}");
+            return None;
+        }
+    });
+
+    Some(module)
+}
+
+fn parse_lean_version(version: &str) -> Option<String> {
+    let version = version.split_whitespace().last()?;
+    Some(version.to_string())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::test::ModuleRenderer;
+    use nu_ansi_term::Color;
+    use std::fs::File;
+    use std::io;
+
+    #[test]
+    fn test_folder_without_lean_file() -> io::Result<()> {
+        let dir = tempfile::tempdir()?;
+
+        let actual = ModuleRenderer::new("lean").path(dir.path()).collect();
+
+        let expected = None;
+
+        assert_eq!(actual, expected);
+        dir.close()
+    }
+
+    #[test]
+    fn test_folder_with_lean_file() -> io::Result<()> {
+        let dir = tempfile::tempdir()?;
+        File::create(dir.path().join("hello.lean"))?.sync_all()?;
+
+        let actual = ModuleRenderer::new("lean").path(dir.path()).collect();
+
+        let expected = Some(format!(
+            "via {}",
+            Color::Rgb(53, 145, 253).bold().paint("∀ v1.0.0 ")
+        ));
+
+        assert_eq!(actual, expected);
+        dir.close()
+    }
+
+    #[test]
+    fn test_folder_with_lean_toml() -> io::Result<()> {
+        let dir = tempfile::tempdir()?;
+        File::create(dir.path().join("lakefile.toml"))?.sync_all()?;
+
+        let actual = ModuleRenderer::new("lean").path(dir.path()).collect();
+
+        let expected = Some(format!(
+            "via {}",
+            Color::Rgb(53, 145, 253).bold().paint("∀ v1.0.0 ")
+        ));
+
+        assert_eq!(actual, expected);
+        dir.close()
+    }
+
+    #[test]
+    fn test_parse_lean_version() {
+        let version = "lean 1.0.0";
+        assert_eq!(parse_lean_version(version), Some("1.0.0".to_string()));
+    }
+}
