a2d39e45b5a528b13ca1dfeaa1d70b3b0fdf7341
Alexis (Poliorcetics) Bourget <ab_contribs@poliorcetiq.eu>
2026-07-20 14:49:38 +0200

    modules: jj_change: add rendering tests

--- src/modules/jj_change.rs
+++ src/modules/jj_change.rs
@@ -68,3 +68,134 @@ fn suffix(id: &str, prefix_size: u8, hash_length: u8) -> Option<&str> {
     id.get(prefix_size.into()..end.into())
         .filter(|s| !s.is_empty())
 }
+
+#[cfg(test)]
+mod tests {
+    use nu_ansi_term::{Color, Style};
+    use toml::toml;
+
+    use crate::context::JJRepo;
+
+    use super::{prefix, suffix};
+
+    use crate::modules::jj_bookmark::tests::Tester;
+
+    // == Helper tests ==
+
+    #[test]
+    fn test_prefix() {
+        assert_eq!(prefix("", 42), None);
+        assert_eq!(prefix("abc", 0), None);
+        assert_eq!(prefix("123", 12), None);
+
+        assert_eq!(prefix("12345678", 4), Some("1234"));
+    }
+
+    #[test]
+    fn test_suffix() {
+        assert_eq!(suffix("", 24, 42), None);
+        assert_eq!(suffix("", 42, 24), None);
+        assert_eq!(suffix("abc", 4, 3), None);
+        assert_eq!(suffix("123", 2, 2), None);
+        assert_eq!(suffix("12345678", 4, 10), None);
+
+        assert_eq!(suffix("12345678", 4, 7), Some("567"));
+    }
+
+    // == Render tests ==
+
+    fn tester(repo: &'static str) -> Tester {
+        Tester::new("jj_change").repo(repo)
+    }
+
+    #[test]
+    fn test_render_basics() {
+        Tester::basic_tests("jj_change");
+    }
+
+    #[test]
+    fn test_render_default_config() {
+        tester(JJRepo::BASE)
+            .expected(format!(
+                "{}{} ",
+                Color::Green.bold().paint("pvt"),
+                Style::new().dimmed().paint("xwmv")
+            ))
+            .render();
+    }
+
+    #[test]
+    fn test_render_style() {
+        tester(JJRepo::BASE)
+            .options(toml! {
+                prefix_style = "italic red"
+                suffix_style = "blue bold"
+            })
+            .expected(format!(
+                "{}{} ",
+                Color::Red.italic().paint("pvt"),
+                Color::Blue.bold().paint("xwmv")
+            ))
+            .render();
+    }
+
+    #[test]
+    fn test_render_format() {
+        tester(JJRepo::BASE)
+            .options(toml! {
+                format = "$change \\($commit\\)"
+                prefix_style = ""
+                suffix_style = ""
+            })
+            .expected("pvtxwmv (30363e4)")
+            .render();
+    }
+
+    #[test]
+    fn test_render_variables() {
+        tester(JJRepo::BASE)
+            .options(toml! {
+                format = "$change_prefix $change_suffix \\($commit_prefix $commit_suffix\\)"
+            })
+            .expected("pvt xwmv (3036 3e4)")
+            .render();
+    }
+
+    #[test]
+    fn test_render_modified_change_hash_length() {
+        tester(JJRepo::BASE)
+            .options(toml! {
+                format = "$change_prefix $change_suffix"
+                change_hash_length = 10
+            })
+            .expected("pvt xwmvttt")
+            .render();
+
+        tester(JJRepo::BASE)
+            .options(toml! {
+                format = "$change_prefix $change_suffix"
+                change_hash_length = 2
+            })
+            .expected("pvt ")
+            .render();
+    }
+
+    #[test]
+    fn test_render_modified_commit_hash_length() {
+        tester(JJRepo::BASE)
+            .options(toml! {
+                format = "$commit_prefix $commit_suffix"
+                commit_hash_length = 12
+            })
+            .expected("3036 3e463b3a")
+            .render();
+
+        tester(JJRepo::BASE)
+            .options(toml! {
+                format = "$commit_prefix $commit_suffix"
+                commit_hash_length = 2
+            })
+            .expected("3036 ")
+            .render();
+    }
+}
