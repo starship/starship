2db2ac143304676e850e3da44c18d89d84cf0803
Trent Schmidt <trent.schmidt@gmail.com>
2026-07-26 20:32:21 -0700

    feat(claude_model): add thinking symbol

--- src/context.rs
+++ src/context.rs
@@ -30,7 +30,7 @@ use terminal_size::terminal_size;
 
 pub use crate::utils::env::Env;
 pub use crate::utils::statusline::{
-    ClaudeCodeData, ContextWindow, CostInfo, CurrentUsage, ModelInfo, Workspace,
+    ClaudeCodeData, ContextWindow, CostInfo, CurrentUsage, ModelInfo, ThinkingInfo, Workspace,
 };
 
 /// Context contains data or common methods that may be used by multiple modules.
