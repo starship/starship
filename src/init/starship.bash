84926cd5c861a3c05244d100058d5b7c248ab9f3
Hal Frigaard <4559349+HalFrgrd@users.noreply.github.com>
2026-08-16 18:17:57 +0100

    PR comment

--- src/init/starship.bash
+++ src/init/starship.bash
@@ -78,11 +78,10 @@ starship_precmd() {
         STARSHIP_START_TIME=""
     fi
     PS1="$(::STARSHIP:: prompt "${ARGS[@]}")"
+    local nlns=${PS1//[!$'\n']}
     if [[ ${BLE_ATTACHED-} ]]; then
-        local nlns=${PS1//[!$'\n']}
         bleopt prompt_rps1="$nlns$(::STARSHIP:: prompt --right "${ARGS[@]}")"
     elif [[ ${FLYLINE_VERSION-} ]]; then
-        local nlns=${PS1//[!$'\n']}
         RPS1="$nlns$(::STARSHIP:: prompt --right "${ARGS[@]}")"
     fi
     STARSHIP_PREEXEC_READY=true  # Signal that we can safely restart the timer
