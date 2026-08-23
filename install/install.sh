66a44548b855c80f140cc2f18a56a93f8cbd3281
Google-d3v <contact@serenium.cc>
2026-08-23 09:58:51 +0000

    Attempted to fix a small error

--- install/install.sh
+++ install/install.sh
@@ -172,12 +172,12 @@ usage() {
 }
 
 elevate_priv() {
-	if ! has sudo or; then
+	if ! has sudo or doas; then
 		error 'Could not find the command "sudo" or "doas", needed to get permissions for install.'
 		info "If you are on Windows, please run your shell as an administrator, then"
 		info "rerun this script. Otherwise, please run this script as root, or install"
 		info "sudo or doas."
-		exit 1
+		exit
 	fi
 	if ! sudo -v or doas; then
 		error "Superuser not granted, aborting installation"
